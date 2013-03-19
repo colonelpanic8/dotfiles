d3ac8e0bb2fea42c11944caa74b6768ca104ed7b
Ivan Malison <IvanMalison@gmail.com>
2013-03-19 00:41:14 -0700

    Added testify completion submodule bump added excludes.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE}")"
 function doIt() {
-    rsync --exclude "oh-my-zsh" --exclude "tmux-powerline" --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude "setup.sh" -av . ~
+    rsync --exclude "Monaco-Powerline.otf" --exclude "oh-my-zsh" --exclude "tmux-powerline" --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude "setup.sh" -av . ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
     doIt
