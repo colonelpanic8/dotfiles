3961734e8d9b06f7be774cb79491b732e924bf02
Ivan Malison <IvanMalison@gmail.com>
2013-03-25 10:37:38 -0700

    Added whitespace changes to init.el.

--- init.el
+++ init.el
@@ -175,6 +175,12 @@
 (require 'whitespace)
 (require 'rainbow-delimiters)
 
+;; make whitespace-mode use just basic coloring
+(setq whitespace-style (quote (spaces tabs newline space-mark tab-mark newline-mark)))
+(setq whitespace-display-mappings
+      '((space-mark 32 [183] [46]) 
+        (tab-mark 9 [9655 9] [92 9])))
+
 (set-face-background 'mode-line "black")
 (set-face-foreground 'mode-line "white")
 (set-face-background 'mode-line-inactive "black")
