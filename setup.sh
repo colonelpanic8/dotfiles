b3d5da867d906a3a0a47517a1ebbf290abdd6966
Ivan Malison <IvanMalison@gmail.com>
2013-04-01 01:31:41 +0000

    Updating things for web_start.

--- setup.sh
+++ setup.sh
@@ -1,48 +1,18 @@
 #!/bin/bash
+# Assumes that gcc, make and git as well as a package manager (brew,
+# apt-get) is installed on the system.
+
 # Go to the source directory of this script.
 cd "$(dirname "${BASH_SOURCE}")"
 
-function install_essentials() {
-    case `uname` in
-	'Darwin')
-	    hash gcc &>/dev/null
-	    if [ $? -ne 0 ]
-	    then
-		echo "gcc not found."
-		exit
-		case `sw_vers | grep ProductVersion | awk '{print $2}' | awk 'BEGIN{FS="."}{print $2}'` in
-		    '8')
-			echo 'Mountain Lion Detected.'
-			;;
-		    '7')
-			echo 'Lion Detected.'
-			;;
-		    *)
-			exit -1
-			;;
-		esac
-	    else
-		echo "gcc found."
-		source ~/.osx
-	    fi
-	    hash brew &>/dev/null && echo "brew found." || ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
-	    source .brew
-	    ;;
-	'Linux')
-            echo "Linux detected."
-	    hash apt-get &>/dev/null || (echo 'apt-get is missing.' && exit)
-            echo "apt-get found."
-	    sudo apt-get -y install build-essential
-            sudo apt-get -y install git
-            sudo git clone git@github.com:IvanMalison/dotfiles.git
-            cd dotfiles
-            source .debian
-	    ;;
-	*)
-	    echo "Operating System not recognized; aborting."
-	    exit
-    esac
-}
+case `uname` in
+    'Darwin')
+	source .brew
+	;;
+    'Linux')
+	source .apt-get
+        ;;
+esac
 
 function install_python_packages() {
     sudo -v
