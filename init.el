a3e35bec30ed57a60bf4cffc32a0dd10e0170339
Ivan Malison <IvanMalison@gmail.com>
2014-03-21 11:31:41 -0700

    bump emacs-testify pointer.

--- init.el
+++ init.el
@@ -268,3 +268,9 @@
 ;; =============================================================================
 ;;                                                                     Customize
 ;; =============================================================================
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((python-indent . 4) (whitespace-line-column . 80) (lexical-binding . t)))))
