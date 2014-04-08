2e8a7c572cc50717e68d4bde809bb6ee6ccb445e
Ivan Malison <IvanMalison@gmail.com>
2014-04-08 04:18:33 -0700

    Fix zsh prompt to exapand every time.

--- dotfiles/lib/zsh.sh
+++ dotfiles/lib/zsh.sh
@@ -10,6 +10,7 @@ compinit
 autoload -U edit-command-line
 zle -N edit-command-line
 bindkey '\C-x\C-e' edit-command-line
+setopt PROMPT_SUBST
 
 
 function current_directory() {
@@ -26,4 +27,4 @@ function git_prompt_info () {
     fi
 }
 
-PROMPT="%{$FG[040]%}%n%{$reset_color%} %{$FG[239]%}at%{$reset_color%} %{$FG[033]%}$(hostname -s)%{$reset_color%} %{$FG[239]%}in%{$reset_color%} %{$terminfo[bold]$FG[226]%}$(current_directory)%{$reset_color%}$(git_prompt_info) %{$FG[239]%}with $(colored_sandbox_string)%{$FG[255]%} "
+PROMPT='%{$FG[040]%}%n%{$reset_color%} %{$FG[239]%}at%{$reset_color%} %{$FG[033]%}$(hostname -s)%{$reset_color%} %{$FG[239]%}in%{$reset_color%} %{$terminfo[bold]$FG[226]%}$(current_directory)%{$reset_color%}$(git_prompt_info) %{$FG[239]%}with $(colored_sandbox_string)%{$FG[255]%} '
