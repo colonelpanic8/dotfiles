8d31df60461553cbabf2e38e1b6d609e40874e5f
Ivan Malison <IvanMalison@gmail.com>
2014-04-09 13:29:07 -0700

    Add terminal info that allows emacs to use full color spectrum.

--- dotfiles/lib/shellrc/exports.sh
+++ dotfiles/lib/shellrc/exports.sh
@@ -1,3 +1,8 @@
+if [[ $COLORTERM = gnome-* && $TERM = xterm ]] && infocmp gnome-256color >/dev/null 2>&1; then
+    export TERM=gnome-256color
+elif infocmp xterm-256color >/dev/null 2>&1; then
+    export TERM=xterm-256color
+fi
 # Make emacs the default editor.
 export EDITOR="emacsclient"
 export ALTERNATE_EDITOR=""
