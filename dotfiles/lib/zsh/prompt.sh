1f5123e2fbf70781554efd23a502f6ed2dd466b9
Ivan Malison <IvanMalison@gmail.com>
2014-04-25 13:18:15 -0700

    Move stuff from spectrum.sh into prompt.sh

--- dotfiles/lib/zsh/prompt.sh
+++ dotfiles/lib/zsh/prompt.sh
@@ -1,4 +1,31 @@
+##### COLOR SETUP
 autoload -U colors && colors
+# P.C. Shyamshankar <sykora@lucentbeing.com>
+# Copied from
+# http://github.com/sykora/etc/blob/master/zsh/functions/spectrum/
+typeset -Ag FX FG BG
+FX=(
+    reset     "%{[00m%}"
+    bold      "%{[01m%}" no-bold      "%{[22m%}"
+    italic    "%{[03m%}" no-italic    "%{[23m%}"
+    underline "%{[04m%}" no-underline "%{[24m%}"
+    blink     "%{[05m%}" no-blink     "%{[25m%}"
+    reverse   "%{[07m%}" no-reverse   "%{[27m%}"
+)
+
+for color in {000..255}; do
+    FG[$color]="%{[38;5;${color}m%}"
+    BG[$color]="%{[48;5;${color}m%}"
+done
+
+# Show all 256 colors with color number
+function spectrum_ls() {
+  for code in {000..255}; do
+    print -P -- "$code: %{$FG[$code]Test%f%}"
+  done
+}
+#####
+
 function current_directory() {
     local PWD=$(pwd)
     echo "${PWD/#$HOME/~}"
@@ -29,21 +56,21 @@ function sandbox_prompt() {
     fi
 }
 
-function prompt_use_custom_colors() {
+function prompt_custom_colors() {
     export USERNAME_COLOR="$FG[040]"
     export SEPARATOR_COLOR="$FG[239]"
     export HOSTNAME_COLOR="$FG[033]"
     export CURRENT_DIRECTORY_COLOR="$FG[226]"
 }
 
-function prompt_use_basic_colors() {
+function prompt_basic_colors() {
     export USERNAME_COLOR="$fg[blue]"
     export SEPARATOR_COLOR="$fg[black]"
     export HOSTNAME_COLOR="$fg[green]"
     export CURRENT_DIRECTORY_COLOR="$fg[yellow]"
 }
 
-function prompt_grey_separator() {
+function prompt_basic_colors_with_grey_separator() {
     export USERNAME_COLOR="$fg[blue]"
     export SEPARATOR_COLOR="$FG[239]"
     export HOSTNAME_COLOR="$fg[green]"
@@ -58,7 +85,7 @@ function separator() {
     print_with_color "$1" "$SEPARATOR_COLOR"
 }
 
-prompt_use_custom_colors
+prompt_basic_colors_with_grey_separator
 PROMPT='╭─% $(print_with_color "%n" "$USERNAME_COLOR") $(separator "at") $(print_with_color "`hostname -s`" "$HOSTNAME_COLOR") $(separator "in") $(print_with_color "`current_directory`" "$terminfo[bold]$CURRENT_DIRECTORY_COLOR")$(git_prompt_info)$(sandbox_prompt)
 ╰─± '
 
