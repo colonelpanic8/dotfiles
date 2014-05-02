7477fa6a391f6fb36011229c7ea73d557e917ee9
Ivan Malison <IvanMalison@gmail.com>
2014-05-01 18:11:18 -0700

    Fix bootstrapping.sh.

--- resources/bootstrapping.sh
+++ resources/bootstrapping.sh
@@ -1,5 +1,5 @@
 readlink_command='readlink'
 command -v greadlink > /dev/null && readlink_command="greadlink"
 function dotfiles_abspath() {
-    echo "$(${readlink_command} -f "$BASH_SOURCE" | xargs dirname)"
+    echo "$(${readlink_command} -f "$BASH_SOURCE" | xargs dirname | xargs dirname)"
 }
