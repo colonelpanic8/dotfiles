8867f3db1ed55590c032f6329723867b316cb1f3
Ivan Malison <IvanMalison@gmail.com>
2014-09-21 19:09:03 -0700

    Tweaks.

--- dotfiles/lib/shellrc.sh
+++ dotfiles/lib/shellrc.sh
@@ -10,6 +10,8 @@ add_to_back_of_path "$HOME/.local/lib/python2.6/site-packages"
 hash brew 2>/dev/null && add_to_front_of_path "$(brew --prefix coreutils)/libexec/gnubin"
 add_to_front_of_path "/usr/local/bin"
 
+add_to_back_of_path "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/"
+
 for filename in ~/.lib/shellrc/*; do
     source $filename
 done
