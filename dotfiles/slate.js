590f08a6763b5d29508deb77f5f3320e41e36bda
Ivan Malison <IvanMalison@gmail.com>
2014-10-24 06:19:23 -0700

    Add hyper key.

--- dotfiles/slate.js
+++ dotfiles/slate.js
@@ -1,16 +1,19 @@
 var hint = slate.op("hint", {
   "characters" : "ASDFGHJKLQWERTYUIOPCVBN"
 });
+
 var grid = slate.op("grid", {
   grids: {
     "1920x1080": {"width": 8, "height": 6}
   }
 });
 
+var hyper = ":ctrl;shift;alt;cmd";
 slate.bindAll({
   "esc:cmd": hint,
   "space:alt": grid
 });
+slate.bind("h" + hyper, grid);
 
 slate.configAll({
   windowHintsIgnoreHiddenWindows: false,
