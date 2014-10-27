d3aaeb420fd7946143b2cf375d83211443c9a561
Ivan Malison <IvanMalison@gmail.com>
2014-10-27 03:21:33 -0700

    bump .emacs.d made ec alias for operning files in existing emacs frame.

--- dotfiles/lib/shellrc/emacs.sh
+++ dotfiles/lib/shellrc/emacs.sh
@@ -1,14 +1,15 @@
 alias emacs='cemacs'
 alias cemacs='_emacs -c -n'
 alias temacs='_emacs -t'
+alias ec='_emacs -n '
 
 function _emacs {
     local directory="$(git rev-parse --show-toplevel 2> /dev/null || pwd)"
     local server_name="$(_dot_directory $directory)"
-    test -z "$(ps aux | grep -v grep | grep -i "emacs -nw --daemon=.*$server_name$")" &&  echo "Starting emacs with server name \'$server_name'" && \emacs --daemon="$server_name"
+    test -z "$(ps aux | grep -v grep | grep -i "emacs -nw --daemon=.*$server_name$")" &&  echo "Starting emacs with server name '$server_name'" && \emacs --daemon="$server_name"
     emacsclient $* --server-file="$server_name"
 }
 
 function _dot_directory {
-    echo $1 | sed "s:/:.:g"
+    echo $1 | sed "s:/:.:g" 
 }
