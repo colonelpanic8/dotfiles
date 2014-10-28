04b9d61696c444859065a4d3d3ae05b9e3e76ff5
Ivan Malison <IvanMalison@gmail.com>
2014-10-28 03:37:53 -0700

    Move a bunch of stuff around. fix fullscreen emacs issue.

--- dotfiles/lib/shellrc/colors.sh
+++
@@ -1,3 +0,0 @@
-dircolors_file="$HOME/.dircolors"
-
-[ ! -z "$SHELL" ] && test -r $dircolors_files  && eval "$(dircolors $dircolors_file)"
--- dotfiles/lib/zsh/completion.sh
+++ dotfiles/lib/zsh/completion.sh
@@ -94,3 +94,10 @@ zstyle ':completion:*:(ssh|scp):*:hosts-ipaddr' ignored-patterns \
       '^<->.<->.<->.<->' '127.0.0.<->'
 zstyle ':completion:*:(ssh|scp):*:users' ignored-patterns \
       adm bin daemon halt lp named shutdown sync
+
+zstyle -e ':completion:*:(ssh|scp):*' hosts 'reply=(
+      ${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) \
+                      /dev/null)"}%%[# ]*}//,/ }
+      ${=${(f)"$(cat /etc/hosts(|)(N) <<(ypcat hosts 2>/dev/null))"}%%\#*}
+      ${=${${${${(@M)${(f)"$(<~/.ssh/config)"}:#Host *}#Host }:#*\**}:#*\?*}}
+      )'
