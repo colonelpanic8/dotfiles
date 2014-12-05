1991164f4a30ace84859c967a31e5b776f10e22e
Ivan Malison <IvanMalison@gmail.com>
2014-12-05 15:05:18 -0800

    tweak org stuff, more helm-org-habits work.

--- dotfiles/emacs.d/load.d/helm-org-headlines.el
+++ dotfiles/emacs.d/load.d/helm-org-headlines.el
@@ -2,7 +2,6 @@
   (interactive)
   (helm :sources (helm-source-org-headlines-for-files org-agenda-files)))
 
-
 (defun helm-org-goto-marker (marker)
   (switch-to-buffer (marker-buffer marker))
   (goto-char (marker-position marker))
@@ -13,7 +12,7 @@
   (unless min-depth (setq min-depth 1))
   (unless max-depth (setq max-depth 8))
   (helm-build-sync-source "Org Headlines"
-    :candidates (helm-org-get-candidates filenames)
+    :candidates (helm-org-get-candidates filenames min-depth max-depth)
     :action 'helm-org-goto-marker
     :action-transformer
     (lambda (actions candidate)
@@ -25,22 +24,25 @@
 (defun helm-org-headline-refile (marker)
   (with-helm-current-buffer
     (org-cut-subtree))
-  (helm-org-goto-marker marker)
-  (goto-char (marker-position marker))
-  (let (destination-level (org-current-level))
+  (let ((target-level (with-current-buffer (marker-buffer marker)
+                       (goto-char (marker-position marker))
+                       (org-current-level))))
+    (helm-org-goto-marker marker)
     (org-end-of-subtree t t)
-    (org-paste-subtree (+ destination-level 1))))
+    (org-paste-subtree (+ target-level 1))))
 
-(defun helm-org-get-candidates (filenames)
+(defun helm-org-get-candidates (filenames min-depth max-depth)
   (-flatten
    (mapcar (lambda (filename)
              (helm-get-org-candidates-in-file
               filename min-depth max-depth))
-           org-agenda-files)))
+           filenames)))
 
 (defun helm-get-org-candidates-in-file (filename min-depth max-depth)
   (with-current-buffer (find-file-noselect filename)
     (save-excursion
       (beginning-of-buffer)
       (cl-loop while (re-search-forward org-complex-heading-regexp nil t)
+               if (let ((num-stars (length (match-string-no-properties 1))))
+                    (and (>= num-stars min-depth) (<= num-stars max-depth)))
                collect `(,(match-string-no-properties 0) . ,(point-marker))))))
