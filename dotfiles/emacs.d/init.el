4990819296d68f44ae9a5357f91297b5026e7c3d
Ivan Malison <IvanMalison@gmail.com>
2014-12-09 01:47:39 -0800

    move mu4e back into init.el

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -566,6 +566,7 @@ The current directory is assumed to be the project's root otherwise."
     (use-package org-projectile :ensure t)
     (setq org-habit-graph-column 50)
     (setq org-habit-show-habits-only-for-today t)
+    (setq org-lowest-priority 69)
     (unless (boundp 'org-gtd-file)
       (defvar org-gtd-file "~/org/gtd.org"))
     (unless (boundp 'org-habits-file)
@@ -700,6 +701,68 @@ The current directory is assumed to be the project's root otherwise."
   :config
   (progn
     (use-package erc-colorize :ensure t) (erc-colorize-mode 1)))
+
+(use-package s :ensure t)
+(add-to-list 'load-path (s-trim (shell-command-to-string "mu4e_directory")))
+
+(use-package mu4e
+  :commands mu4e
+  :config
+  (progn
+    (setq mu4e-compose-complete-only-after nil)
+    (setq mu4e-maildir "~/Mail")
+
+    (setq mu4e-drafts-folder "/[Gmail].Drafts")
+    (setq mu4e-sent-folder   "/[Gmail].Sent Mail")
+    (setq mu4e-trash-folder  "/[Gmail].Trash")
+
+    ;; don't save message to Sent Messages, Gmail/IMAP takes care of this
+    (setq mu4e-sent-messages-behavior 'delete)
+
+    ;; setup some handy shortcuts
+    ;; you can quickly switch to your Inbox -- press ``ji''
+    ;; then, when you want archive some messages, move them to
+    ;; the 'All Mail' folder by pressing ``
+
+    ;; allow for updating mail using 'U' in the main view:
+    (setq mu4e-get-mail-command "offlineimap")
+    ;; show images
+    (setq mu4e-show-images t)
+    (add-hook 'mu4e-compose-mode-hook
+              (defun my-do-compose-stuff () (flyspell-mode)))
+    (setq mu4e-update-interval (* 60 20))
+
+    ;; ;; something about ourselves
+    ;; (setq
+    ;;    mu4e-compose-signature
+    ;;     (concat
+    ;;       "Foo X. Bar\n"
+    ;;       "http://www.example.com\n"))
+
+    ;; sending mail -- replace USERNAME with your gmail username
+    ;; also, make sure the gnutls command line utils are installed
+    ;; package 'gnutls-bin' in Debian/Ubuntu
+
+    (require 'smtpmail)
+    ;; (setq message-send-mail-function 'smtpmail-send-it
+    ;;    starttls-use-gnutls t
+    ;;    smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+    ;;    smtpmail-auth-credentials
+    ;;      '(("smtp.gmail.com" 587 "USERNAME@gmail.com" nil))
+    ;;    smtpmail-default-smtp-server "smtp.gmail.com"
+    ;;    smtpmail-smtp-server "smtp.gmail.com"
+    ;;    smtpmail-smtp-service 587)
+
+    ;; alternatively, for emacs-24 you can use:
+    (setq message-send-mail-function 'smtpmail-send-it
+          smtpmail-stream-type 'starttls
+          smtpmail-default-smtp-server "smtp.gmail.com"
+          smtpmail-smtp-server "smtp.gmail.com"
+          smtpmail-smtp-service 587)
+
+    ;; don't keep message buffers around
+    (setq message-kill-buffer-on-exit t)))
+
 
 (use-package sauron
   :ensure t
@@ -864,6 +927,9 @@ The current directory is assumed to be the project's root otherwise."
 ;; =============================================================================
 
 (setq edebug-trace t)
+
+(use-package paredit
+  :ensure t)
 
 (use-package elisp-slime-nav
   :ensure t
