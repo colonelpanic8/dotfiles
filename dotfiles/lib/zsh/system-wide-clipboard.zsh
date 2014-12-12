faa4fab7c0cdcebe9e86cfa0084f293fe75e873c
Ivan Malison <IvanMalison@gmail.com>
2014-12-12 00:01:10 -0800

    Don't call in to xclip when $DISPLAY isn't available.

--- dotfiles/lib/zsh/system-wide-clipboard.zsh
+++ dotfiles/lib/zsh/system-wide-clipboard.zsh
@@ -1,32 +1,32 @@
 pb-kill-line () {
   zle kill-line
-  echo -n $CUTBUFFER | clipboard
+  echo -n $CUTBUFFER | oscopy
 }
 
 pb-kill-whole-line () {
   zle kill-whole-line
-  echo -n $CUTBUFFER | clipboard
+  echo -n $CUTBUFFER | oscopy
 }
 
 pb-backward-kill-word () {
   zle backward-kill-word
-  echo -n $CUTBUFFER | clipboard
+  echo -n $CUTBUFFER | oscopy
 }
 
 pb-kill-word () {
   zle kill-word
-  echo -n $CUTBUFFER | clipboard
+  echo -n $CUTBUFFER | oscopy
 }
 
 pb-kill-buffer () {
   zle kill-buffer
-  echo -n $CUTBUFFER | clipboard
+  echo -n $CUTBUFFER | oscopy
 }
 
 pb-copy-region-as-kill-deactivate-mark () {
   zle copy-region-as-kill
   zle set-mark-command -n -1
-  echo -n $CUTBUFFER | clipboard
+  echo -n $CUTBUFFER | oscopy
 }
 
 pb-yank () {
