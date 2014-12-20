361017006554cb2d2cbd207fec5bae5597cfd0ab
Ivan Malison <IvanMalison@gmail.com>
2014-12-20 01:23:42 -0800

    tweak mu4e syncing.

--- tasks/osx.py
+++ tasks/osx.py
@@ -137,6 +137,13 @@ def cl_tools(ctx):
 
 
 @ctask
+def fix_htop(ctx):
+    real_htop_location = ctx.run("readlink -f $(brew --prefix htop)").stdout.strip() + "/bin/htop"
+    ctx.run("chmod 6555 {0}".format(real_htop_location))
+    ctx.run("sudo chown root {0}".format(real_htop_location))
+
+
+@ctask
 def iTerm(ctx):
     library_plist = os.path.join(os.path.expanduser("~"), "Library",
                                  "Preferences", "com.googlecode.iterm2.plist")
