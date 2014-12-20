15e12f06b5077570cfae64694f80ac0aa935c551
Ivan Malison <IvanMalison@gmail.com>
2014-12-20 01:23:42 -0800

    tweak mu4e syncing.

--- dotfiles/lib/shellrc/mu4e.sh
+++ dotfiles/lib/shellrc/mu4e.sh
@@ -1,21 +1,26 @@
 MAILDIR="$HOME/Mail"
 SYNC_STAMP="$HOME/.mail-sync"
+APP_ICON="$(dotfiles_directory)/resources/gmail_logo.png"
+SYNC_COMMAND="offlineimap -f INBOX"
 
 function mu4e_alert_for_filename {
     local message="$(mu view $1 | grep -B 10 Date)"
     local title="$(echo $message | grep From | sed 's/From: //')"
     local subject="$(echo $message | grep Subject | sed 's/Subject: //')"
-    local view_file_command="$(which zsh) -c \"refresh_config && mu4e_view_message_from_file $1\""
+    local mu4e_message_id="$(mu4e_get_msg_id_from_file $1)"
+    local view_file_command="$(which zsh) -c \"refresh_config && mu4e_view_message $mu4e_message_id\""
+    local app_icon_argument=''
+    test -e "$APP_ICON" && app_icon_argument="-appIcon '$APP_ICON'"
     reattach-to-user-namespace $(which terminal-notifier) \
-	-title "$title" \
-	-message "$subject" \
-	-execute "$view_file_command" \
-	-activate "org.gnu.Emacs" \
-	-appIcon "$(dotfiles_directory)/resources/gmail_logo.png"
+        -title "$title" \
+        -message "$subject" \
+        -execute "$view_file_command" \
+        -activate "org.gnu.Emacs" \
+        $app_icon_argument
 }
 
 function mu4e_update_mail {
-    offlineimap
+    eval $SYNC_COMMAND
     mu4e_update_index
     mu4e_alerts
 }
@@ -34,7 +39,7 @@ function mu4e_alerts {
 }
 
 function mu4e_get_msg_id_from_file {
-    mu view $1 -o sexp | grep "message-id" | get_cols ' -1'
+    mu view $1 -o sexp | grep "message-id" | get_cols ' -1' | sed 's/"//g'
 }
 
 function mu4e_view_message_from_file {
@@ -42,6 +47,5 @@ function mu4e_view_message_from_file {
 }
 
 function mu4e_view_message {
-    local elisp="(mu4e-view-message-with-msgid "$1")"
-    execute_elisp "$elisp"
+    execute_elisp "(mu4e-view-message-with-msgid \"$1\")"
 }
