a174d3105ee511e1985e0d3227e33d4886becd85
Ivan Malison <IvanMalison@gmail.com>
2014-12-27 06:05:13 -0500

    Add the right repository for emacs.d (was just adding dotfiles)

--- resources/add_emacs.d_aliases.sh
+++ resources/add_emacs.d_aliases.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 git config alias.ped "subtree push --prefix=dotfiles/emacs.d emacs.d master"
 git config alias.ued "subtree pull --prefix=dotfiles/emacs.d emacs.d master"
-git remote add emacs.d git@github.com:IvanMalison/dotfiles.git
+git remote add emacs.d git@github.com:IvanMalison/.emacs.d.git
