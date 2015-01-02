7906fc0f0f014e90cb3eafe436498f5a1dd43a7c
Ivan Malison <IvanMalison@gmail.com>
2015-01-02 11:52:35 -0800

    zsh history changes.

--- dotfiles/lib/zsh/history.sh
+++ dotfiles/lib/zsh/history.sh
@@ -1,15 +1,13 @@
 ## Command history configuration
-HISTFILE=$HOME/.zsh_history
+HISTFILE="$(readlink -f $HOME/.zsh_history)"
 HISTSIZE=100000
 SAVEHIST=100000
 
-setopt append_history
 setopt extended_history
 setopt hist_expire_dups_first
 setopt hist_ignore_dups # ignore duplication command history list
 setopt hist_ignore_space
 setopt hist_verify
-setopt inc_append_history
 setopt share_history # share command history data
 
 # TODO: Explain what some of this does..
