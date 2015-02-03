fdbc6af0a7d3fd01f369d866d24ff07b48bc6c8a
Ivan Malison <IvanMalison@gmail.com>
2015-02-02 16:16:43 -0800

    inc_append_history

--- dotfiles/lib/zsh/history.sh
+++ dotfiles/lib/zsh/history.sh
@@ -5,6 +5,7 @@ SAVEHIST=100000
 
 setopt share_history # share command history data
 setopt extended_history
+setopt inc_append_history
 setopt hist_expire_dups_first
 setopt hist_ignore_dups # ignore duplication command history list
 setopt hist_ignore_space
