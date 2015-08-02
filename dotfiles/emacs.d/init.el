60e0de571e88b6f043bfe06d179c1852d10540bd
Ivan Malison <IvanMalison@gmail.com>
2015-08-02 12:02:09 -0700

    effort global properties

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -701,6 +701,10 @@ The current directory is assumed to be the project's root otherwise."
          ("C-c C-S-t" . org-todo-force-notes))
   :config
   (progn
+    (setq org-global-properties
+          '(quote (("Effort_ALL" . "0:15 0:30 0:45 1:00 2:00 3:00 4:00 5:00 6:00 0:00")
+                   ("STYLE_ALL" . "habit"))))
+    (setq org-columns-default-format "%80ITEM(Task) %10Effort(Effort){:} %10CLOCKSUM")
     (setq helm-org-headings-fontify t)
 
     ;; Enable appointment notifications.
