647d31dc7d2b54c577b1406af8fb111970f06206
Ivan Malison <IvanMalison@gmail.com>
2015-08-04 12:58:51 -0700

    Add NEXT and HANDLED states.

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -1006,8 +1006,8 @@ the same tree node, and the headline of the tree node in the Org-mode file."
     ;; Record changes to todo states
     (setq org-log-into-drawer t)
     (setq org-todo-keywords
-          '((sequence "TODO(t!)" "STARTED(s!)" "WAIT(w!)" "BACKLOG(b!)" "|"
-                      "DONE(d!)" "CANCELED(c!)" "OBVIATED(o!)" "HANDLED(o!)")))
+          '((sequence "TODO(t!)" "NEXT(n!)" "STARTED(s!)" "WAIT(w!)" "BACKLOG(b!)" "|"
+                      "DONE(d!)" "CANCELED(c!)" "OBVIATED(o!)" "HANDLED(h!)" "EXPIRED(e!)")))
     ;; Stop starting agenda from deleting frame setup!
     (setq org-agenda-window-setup 'other-window)
     (define-key mode-specific-map [?a] 'org-agenda)
