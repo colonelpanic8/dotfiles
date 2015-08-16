13bb4059edb0c341aef84a55a9c4286778c83ee0
Ivan Malison <IvanMalison@gmail.com>
2015-08-15 22:13:02 -0700

    add ansi-color-mode.el

---
+++ dotfiles/emacs.d/ansi-color-mode.el
@@ -0,0 +1,11 @@
+(defun ansi-color-apply-on-region-int (beg end)
+  "interactive version of func"
+  (interactive "r")
+  (ansi-color-apply-on-region beg end))
+
+(define-derived-mode fundamental-ansi-mode fundamental-mode "fundamental ansi"
+  "Fundamental mode that understands ansi colors."
+  (require 'ansi-color)
+  (ansi-color-apply-on-region (point-min) (point-max)))
+
+(add-to-list 'auto-mode-alist '("\\.ansi_color\\'" . fundamental-ansi-mode))
