c84450ffcd561d3648eb0196a92d76fd0fab7639
Ivan Malison <IvanMalison@gmail.com>
2015-08-16 14:11:31 -0700

    add source-code-pro-theme

---
+++ dotfiles/emacs.d/source-code-pro-theme.el
@@ -0,0 +1,12 @@
+(deftheme source-code-pro
+  "Theme setting source-code-pro as the default face")
+
+(custom-theme-set-variables
+ 'source-code-pro
+ )
+
+(custom-theme-set-faces
+ 'source-code-pro
+ '(default ((t (:width normal :height 120 :weight normal :slant normal :foundry "nil" :family "Source Code Pro" :background nil)))))
+
+(provide-theme 'source-code-pro)
