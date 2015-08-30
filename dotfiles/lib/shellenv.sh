172e7065caaf841df2d59b16c3d02a681a0d97a0
Ivan Malison <IvanMalison@gmail.com>
2015-08-30 13:25:57 -0700

    Remove /bin/ startup to path

--- dotfiles/lib/shellenv.sh
+++ dotfiles/lib/shellenv.sh
@@ -8,7 +8,6 @@ function _source_shellenv_files {
 }
 
 function _setup_env {
-    PATH="/bin/"
     _path_helper
     idem_add_to_back_of_path "$HOME/.local/lib/python2.6/site-packages"
     idem_add_to_back_of_path "$HOME/.rvm/bin"
