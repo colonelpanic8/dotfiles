dd256a23218b300d0e1c231edff1267ea691d323
Ivan Malison <IvanMalison@gmail.com>
2015-08-29 23:56:56 -0700

    Prompt control based on whether or not is ansi-term or emacs shell

--- dotfiles/lib/zsh.sh
+++ dotfiles/lib/zsh.sh
@@ -9,3 +9,16 @@ alias zshenv="source ~/.zshenv"
 unalias run-help 2> /dev/null 1>/dev/null
 autoload run-help
 HELPDIR=/usr/local/share/zsh/helpfiles
+
+
+set_powerline_prompt
+
+function if_emacs_zsh {
+    if [ -z $(echo "$INSIDE_EMACS" | grep comint) ]; then
+        echo;
+    else
+        set_my_prompt
+    fi
+}
+
+environment_variable_exists INSIDE_EMACS && if_emacs_zsh
