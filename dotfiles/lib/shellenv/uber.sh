38b44d40c48083d86f155155f6683d78b976ad5a
Ivan Malison <IvanMalison@gmail.com>
2015-09-17 23:27:08 -0700

    merchant curl

--- dotfiles/lib/shellenv/uber.sh
+++ dotfiles/lib/shellenv/uber.sh
@@ -37,3 +37,7 @@ goclone() {
   mkdir -p $GOPATH/src/code.uber.internal/$1
   git clone gitolite@code.uber.internal:$1 $GOPATH/src/code.uber.internal/$1
 }
+
+function merchant_curl {
+	tcurl -p localhost:4338 merchant Merchant::$1 -3 "$2" -t ~/go/src/code.uber.internal/everything/merchant/thrift
+}
