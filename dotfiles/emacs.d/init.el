e6feb7cba52d29cf3427d9ddc2e9c42794ddd4e5
Ivan Malison <IvanMalison@gmail.com>
2015-09-26 18:10:15 -0700

    set default encodings

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -479,6 +479,15 @@ The current directory is assumed to be the project's root otherwise."
 
 (setq echo-keystrokes 0.25)
 
+(setq utf-translate-cjk-mode nil) ; disable CJK coding/encoding (Chinese/Japanese/Korean characters)
+(set-language-environment 'utf-8)
+(set-keyboard-coding-system 'utf-8-mac) ; For old Carbon emacs on OS X only
+(setq locale-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(unless (eq system-type 'windows-nt)
+  (set-selection-coding-system 'utf-8))
+(prefer-coding-system 'utf-8)
 ;; =============================================================================
 ;;                                                                   use-package
 ;; =============================================================================
