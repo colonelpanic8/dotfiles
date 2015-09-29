b6c16d1bcf4c88d317ee0a9f7774509ecb290e5b
Ivan Malison <IvanMalison@gmail.com>
2015-09-29 14:11:19 -0700

    set deft-default-extension

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -1220,6 +1220,7 @@ the same tree node, and the headline of the tree node in the Org-mode file."
 (use-package deft
   :config
   (progn
+    (setq deft-default-extension "org")
     (setq deft-extensions '("org"))
     (setq deft-use-filter-string-for-filename t)
     (setq deft-file-naming-rules '((noslash . "_")
