bab589076e2bddbfb55962753c38e6e9f4afb02f
Ivan Malison <IvanMalison@gmail.com>
2015-11-21 19:46:01 -0800

    update multi-lining testing functions

--- dotfiles/emacs.d/load.d/multi-line-test.el
+++ dotfiles/emacs.d/load.d/multi-line-test.el
@@ -10,11 +10,16 @@
 
 (defun test-multi-line-get-markers ()
   (interactive)
-  (message "%d" (length (multi-line-get-markers
-                    (make-instance multi-line-forward-sexp-enter-strategy)
-                    (make-instance multi-line-forward-sexp-find-strategy)))))
+  (let ((markers (multi-line-get-markers
+                  (multi-line-get-enter-strategy multi-line-config)
+                  (multi-line-get-find-strategy multi-line-config))))
+    (message "%d" markers)))
 
 (defclass bars-at-markers () nil)
 
 (defmethod multi-line-respace ((respacer bars-at-markers) index markers)
   (insert "|"))
+
+(defun multiline-test-bars()
+  (interactive)
+  (multi-line-adjust-whitespace (make-instance bars-at-markers)))
