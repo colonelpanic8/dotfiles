4675cd33fef8fb2d0f4e5c027084bff3d4e8ab12
Ivan Malison <IvanMalison@gmail.com>
2016-02-22 11:11:40 -0800

    Always source autojump

--- dotfiles/lib/shellrc.sh
+++ dotfiles/lib/shellrc.sh
@@ -1,4 +1,5 @@
 for filename in ~/.lib/shellrc/*; do
     source $filename
 done
+[[ -s $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
 environment_variable_exists INSIDE_EMACS && inside_emacs_hook
