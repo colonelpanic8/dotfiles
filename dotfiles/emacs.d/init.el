5708686c352eec2b96d35620463e0ac051a70426
Ivan Malison <IvanMalison@gmail.com>
2016-06-06 11:57:27 -0700

    literate init in config.org

---
+++ dotfiles/emacs.d/init.el
@@ -0,0 +1,24 @@
+(require 'package)
+
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+
+(defun ensure-packages-installed (packages)
+  (unless package-archive-contents
+    (package-refresh-contents))
+  (mapcar
+   (lambda (package)
+     (if (package-installed-p package)
+         package
+       (progn (message (format "Installing package %s." package))
+              (package-install package))))
+   packages))
+
+(package-initialize)
+(ensure-packages-installed '(org-plus-contrib))
+
+(org-babel-load-file
+ (concat (file-name-directory load-file-name) "config.org"))
+
+;; Local Variables:
+;; flycheck-disabled-checkers: (emacs-lisp-checkdoc)
+;; End:
