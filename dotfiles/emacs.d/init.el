f6fdc23650343be78e54a58f91f0b05e1009c34d
Ivan Malison <IvanMalison@gmail.com>
2016-06-08 14:40:19 -0700

    use http instead of https for org source by default

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -1,6 +1,6 @@
 (require 'package)
 
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
 (defun ensure-packages-installed (packages)
   (unless package-archive-contents
@@ -16,8 +16,11 @@
 (package-initialize)
 (ensure-packages-installed '(org-plus-contrib))
 
+(setq custom-file "~/.emacs.d/custom-before.el")
+(when (file-exists-p custom-file) (load custom-file))
+
 (org-babel-load-file
- (concat (file-name-directory load-file-name) "README.org") t)
+ (concat (file-name-directory load-file-name) "README.org"))
 
 ;; Local Variables:
 ;; flycheck-disabled-checkers: (emacs-lisp-checkdoc)
