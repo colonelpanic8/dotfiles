2d41071eca6d2f1ccd95bd24f09db4a0562f3339
Ivan Malison <IvanMalison@gmail.com>
2016-06-10 23:42:50 -0700

    make init.el less complicated/sophisticated. boostrapping to get org-mode should not really be necessary since org is included in emacs.

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -1,21 +1,3 @@
-(require 'package)
-
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-
-(defun ensure-packages-installed (packages)
-  (unless package-archive-contents
-    (package-refresh-contents))
-  (mapcar
-   (lambda (package)
-     (if (package-installed-p package)
-         package
-       (progn (message (format "Installing package %s." package))
-              (package-install package))))
-   packages))
-
-(package-initialize)
-(ensure-packages-installed '(org-plus-contrib))
-
 (setq custom-file "~/.emacs.d/custom-before.el")
 (when (file-exists-p custom-file) (load custom-file))
 
