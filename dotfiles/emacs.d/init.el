d14012b6d64866f56bd4f63f8cb819b65037fe34
Ivan Malison <IvanMalison@gmail.com>
2016-06-28 20:42:42 -0700

    Add darwin modifier remapping to init.el
    See comment for details.

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -7,6 +7,12 @@
 (setq custom-file "~/.emacs.d/custom-before.el")
 (when (file-exists-p custom-file) (load custom-file))
 
+;; If this isn't here and there's a problem with init, graphical emacs
+;; is super annoying.
+(when (equal system-type 'darwin)
+  (setq mac-option-modifier 'meta)
+  (setq mac-command-modifier 'super))
+
 (org-babel-load-file
  (concat (file-name-directory load-file-name) "README.org"))
 
