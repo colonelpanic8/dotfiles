0058a21c5f3e44f03e48ee58519d72b9f4dadb0c
Ivan Malison <IvanMalison@gmail.com>
2016-08-16 16:36:11 -0700

    Auto-jump setup

--- dotfiles/lib/shellrc.sh
+++ dotfiles/lib/shellrc.sh
@@ -3,8 +3,15 @@ for filename in ~/.lib/shellrc/*; do
 done
 
 [ -s "/usr/local/bin/virtualenvwrapper.sh" ] && . /usr/local/bin/virtualenvwrapper.sh
-[[ -s $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
+function j() {
+    (( $+commands[brew] )) && {
+        local pfx=$(brew --prefix autojump)
+        [[ -f "$pfx/etc/autojump.sh" ]] && . "$pfx/etc/autojump.sh"
+        j "$@"
+    }
+}
 environment_variable_exists INSIDE_EMACS && inside_emacs_hook
 
 # travis completion
-[ -f "$HOME/.travis/travis.sh" ] && source "$HOME/.travis/travis.sh"
+# XXX: Disabled to reduce performance impact
+# [ -f "$HOME/.travis/travis.sh" ] && source "$HOME/.travis/travis.sh"
