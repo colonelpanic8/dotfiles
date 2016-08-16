dfed16d16891ecb8393e1fba69cfc1101e7ac360
Ivan Malison <IvanMalison@gmail.com>
2016-08-16 11:13:47 -0700

    Remove uber.sh, Move virtualenvwrapper to shellrc

--- dotfiles/lib/shellrc.sh
+++ dotfiles/lib/shellrc.sh
@@ -1,5 +1,6 @@
 for filename in ~/.lib/shellrc/*; do
     source $filename
 done
+[ -s "/usr/local/bin/virtualenvwrapper.sh" ] && . /usr/local/bin/virtualenvwrapper.sh
 [[ -s $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
 environment_variable_exists INSIDE_EMACS && inside_emacs_hook
