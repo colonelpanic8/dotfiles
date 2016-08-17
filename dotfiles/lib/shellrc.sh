45f0c345afc9b2525e918e542b01671e2c1b3f25
Ivan Malison <IvanMalison@gmail.com>
2016-08-16 20:28:29 -0700

    Put ruby and node setup in shellrc.sh

--- dotfiles/lib/shellrc.sh
+++ dotfiles/lib/shellrc.sh
@@ -12,6 +12,11 @@ function j() {
 }
 environment_variable_exists INSIDE_EMACS && inside_emacs_hook
 
+# XXX: This used to be in shellenv and should be moved back once these
+# are faster.
+_node_setup
+_ruby_setup
+
 # travis completion
 # XXX: Disabled to reduce performance impact
 # [ -f "$HOME/.travis/travis.sh" ] && source "$HOME/.travis/travis.sh"
