607defac56d9ef2f3a1b4c7f78714d262c6b10ea
Ivan Malison <IvanMalison@gmail.com>
2016-09-06 11:13:21 -0700

    Add java to arch packages

--- tasks/arch.py
+++ tasks/arch.py
@@ -6,7 +6,7 @@ from .util import RESOURCES_DIRECTORY
 ARCH_PACKAGES = [
     "synergy", "adobe-source-code-pro-fonts", "pyenv", "rbenv", "alsa-utils",
     "spotify", "google-chrome", "autoconf", "automake", "cask", "emacs25-git",
-    "xmobar", "the_silver_searcher",
+    "xmobar", "the_silver_searcher", "jdk8-openjdk", "openjdk8-doc", "openjdk8-src"
 ]
 
 
