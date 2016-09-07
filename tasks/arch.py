b9c395e134cb2352faf1cc26d1c799ac91b01177
Ivan Malison <IvanMalison@gmail.com>
2016-09-07 14:11:08 -0700

    Reindent arch dependencies

--- tasks/arch.py
+++ tasks/arch.py
@@ -1,13 +1,17 @@
+import os
+
 from invoke import task
 
 from .util import RESOURCES_DIRECTORY
 
 
 ARCH_PACKAGES = [
-    "synergy", "adobe-source-code-pro-fonts", "pyenv", "rbenv", "alsa-utils",
-    "patch", "spotify", "google-chrome", "autoconf", "automake", "cask", "emacs25-git",
-    "xmobar", "the_silver_searcher", "jdk8-openjdk", "openjdk8-doc", "openjdk8-src",
-    "scala", "clojure", "go", "ruby", "node", "ghc", "rust",
+    "lib32-alsa-lib", "synergy", "git", "adobe-source-code-pro-fonts", "pyenv",
+    "rbenv", "alsa-utils", "patch", "spotify", "google-chrome", "autoconf",
+    "automake", "cask", "emacs-git", "xmobar", "the_silver_searcher",
+    "jdk8-openjdk", "openjdk8-doc", "openjdk8-src", "scala", "clojure", "go",
+    "ruby", "node", "ghc", "rust", "nodejs", "nvm", "nvidia-settings",
+    "gnome-tweak-tool", "screenfetch", "htop", "tmux", "texlive-most",
 ]
 
 
