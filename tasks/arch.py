a96b425ffbbbfa0e23c2a8f14b7f20d086881ad2
Ivan Malison <IvanMalison@gmail.com>
2016-09-08 13:11:50 -0700

    Update arch packages

--- tasks/arch.py
+++ tasks/arch.py
@@ -5,14 +5,21 @@ from invoke import task
 from .util import RESOURCES_DIRECTORY
 
 
-ARCH_PACKAGES = [
+PACKAGES = [
     "adobe-source-code-pro-fonts", "lib32-alsa-lib", "synergy", "git",
     "pkg-config", "pyenv", "rbenv", "alsa-utils", "patch", "spotify",
     "google-chrome", "autoconf", "automake", "cask", "emacs-git", "xmobar",
     "the_silver_searcher", "jdk8-openjdk", "openjdk8-doc", "openjdk8-src",
     "scala", "clojure", "go", "ruby", "node", "ghc", "rust", "nodejs", "nvm",
     "nvidia-settings", "gnome-tweak-tool", "screenfetch", "htop", "tmux",
-    "texlive-most", "leiningen", "boot", "gnome-settings-daemon"
+    "texlive-most", "leiningen", "boot", "gnome-settings-daemon", "roboto",
+    "accountsservice", "lightdm-webkit-theme-material-git", "openssh",
+    "chrome-remote-desktop", "gtk-theme-arc", "mosh",
+]
+
+
+SERVICES = [
+    "sshd.socket"
 ]
 
 
