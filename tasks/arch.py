c5c30e984d1aacc48a80f82739f2385d6db28bd4
Ivan Malison <IvanMalison@gmail.com>
2016-09-13 02:50:46 -0700

    Add more arch dependencies

--- tasks/arch.py
+++ tasks/arch.py
@@ -18,12 +18,12 @@ PACKAGES = [
     "lightdm-webkit-theme-wisp", "gnome-themes-standard", "zuki-themes",
     "xorg-xfontsel", "gtk2fontsel", "xscreensaver", "networkmanager",
     "network-manager-applet", "feh", "copyq", "imagemagick", "rcm", "rofi",
-    "cabal-install",
+    "cabal-install", "pavucontrol", "lsof", "fbset",
 ]
 
 
 SERVICES = [
-    "sshd.socket", "nvidia-persistenced.service"
+    "sshd.socket", "nvidia-persistenced.service", "NetworkManager.service",
 ]
 
 
