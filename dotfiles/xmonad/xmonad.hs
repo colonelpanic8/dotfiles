af399cd0d8ab8fb5e3326535b86c6a6ec5b278a1
Ivan Malison <IvanMalison@gmail.com>
2016-09-12 20:01:08 -0700

    Update xmonad.hs

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -1,12 +1,24 @@
+import System.IO
+
 import XMonad
 import XMonad.Actions.WindowBringer
 import XMonad.Config()
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
 import XMonad.Util.CustomKeys
+import XMonad.Util.Run(spawnPipe)
 
 main :: IO ()
-main = xmonad defaultConfig
-        { modMask = mod4Mask -- Use Super instead of Alt
+main = do
+  xmproc <- spawnPipe "xmobar"
+  xmonad defaultConfig
+       { modMask = mod4Mask -- Use Super instead of Alt
         , keys = customKeys delkeys inskeys
+        , terminal = "urxvt"
+        , logHook = dynamicLogWithPP xmobarPP
+                    { ppOutput = hPutStrLn xmproc
+                    , ppTitle = xmobarColor "green" "" . shorten 50
+                    }
         }
     where
       delkeys :: XConfig l -> [(KeyMask, KeySym)]
@@ -15,5 +27,5 @@ main = xmonad defaultConfig
       inskeys :: XConfig l -> [((KeyMask, KeySym), X ())]
       inskeys conf =
           [ ((mod4Mask, xK_g), gotoMenu)
-          , ((mod4Mask, xK_b), bringMenu)
+          , ((modM4ask, xK_b), bringMenu)
           ]
