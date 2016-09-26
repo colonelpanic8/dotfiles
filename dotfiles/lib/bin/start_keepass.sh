9cb697d9e5ac34e64c479f342b877bd1b75450f1
Ivan Malison <IvanMalison@gmail.com>
2016-09-26 12:58:05 -0700

    [KeepassX] Tweak some stuff

--- dotfiles/lib/bin/start_keepass.sh
+++ dotfiles/lib/bin/start_keepass.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env zsh
-keepassx --keyfile "$HOME/SparkleShare/config/creds/keepass.key" "$HOME/SparkleShare/config/db.kdbx"
+nohup keepassx --keyfile "$HOME/SparkleShare/config/creds/keepass.key" "$HOME/SparkleShare/config/db.kdbx" &
