0434b75fee7d456b6fcb14ee339ddc063655e239
Ivan Malison <IvanMalison@gmail.com>
2016-09-29 20:36:38 -0700

    [Linux] Update wallpaper daemon

--- dotfiles/lib/bin/wallpaper.sh
+++ dotfiles/lib/bin/wallpaper.sh
@@ -3,13 +3,13 @@
 WALLPAPER_DIR="$HOME/Pictures/wallpaper/"
 
 random_paper() {
-	find "$WALLPAPER_DIR"use -type f | shuf -n1
+	find "$WALLPAPER_DIR"use -type l | shuf -n1
 }
 
 wallpaper() {
 	while true; do
 		feh --bg-center $(random_paper) --bg-scale "$WALLPAPER_DIR"transparent1x1.png
-		sleep 1m
+		sleep 10m
 	done
 }
 
