b757998f4b16ce151cca6f70aa2073cee5611edb
Ivan Malison <IvanMalison@gmail.com>
2016-10-13 00:10:53 -0700

    [XMonad] Move layouts definition

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -35,6 +35,7 @@ myLogHook = fadeInactiveLogHook 0.9
 
 shiftThenView i = W.greedyView i . W.shift i
 
+layouts = multiCol [1, 1] 2 0.01 (-0.5) ||| Full ||| Tall 1 (3/100) (1/2)
 addKeys conf@XConfig {modMask = modm} =
     [ ((modm, xK_p), spawn "rofi -show drun")
     , ((modm .|. shiftMask, xK_p), spawn "rofi -show run")
@@ -79,10 +80,6 @@ addKeys conf@XConfig {modMask = modm} =
              [ (W.greedyView, 0)
              , (W.shift, shiftMask)
              , (shiftThenView, controlMask)]]
-
-layouts = multiCol [1, 1] 2 0.01 (-0.5) ||| Full ||| tiled
-          where
-            tiled = Tall 1 (3/100) (1/2)
 
 myLayoutHook = avoidStruts . smartSpacing 10 . noBorders . minimize
                . boringWindows . mkToggle (MIRROR ?? EOT) $ layouts
