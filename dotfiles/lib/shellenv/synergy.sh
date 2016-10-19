51dadf5bfcdb439b512495839b221ec450052d80
Ivan Malison <IvanMalison@gmail.com>
2016-10-18 20:51:14 -0700

    [Synergy] Fix start_client_at

--- dotfiles/lib/shellenv/synergy.sh
+++ dotfiles/lib/shellenv/synergy.sh
@@ -1,7 +1,7 @@
 SYNERGY_CONF="$HOME/.synergy.conf"
 
 synergy_start_client_at() {
-    ssh "$1" 'test -z $(pgrep synergyc) || synergyc '"$(localip)"'; ps aux | grep synergyc'
+    ssh "$1" '[[ -z "$(pgrep synergyc)" ]] && synergyc '"$(localip)"
 }
 
 synergy_start_server_here() {
