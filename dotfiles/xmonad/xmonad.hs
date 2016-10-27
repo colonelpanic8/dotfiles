b19cc42670f2d54d67e12d02505f35062d046276
Ivan Malison <IvanMalison@gmail.com>
2016-10-27 01:47:17 -0700

    [XMonad] Update target workspaces for manage hook

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -61,10 +61,10 @@ myStartup = spawn "systemctl --user start wm.target"
 -- Manage
 
 myManageHook = composeAll . concat $
-               [ [ emacsSelector --> doShift "0" ]
-               , [ chromeSelector --> doShift "0" ]
-               , [ hangoutsSelector --> doShift "1"]
-               , [ spotifySelector --> doShift "2"]]
+               [ [ emacsSelector --> doShift "1" ]
+               , [ chromeSelector --> doShift "2" ]
+               , [ hangoutsSelector --> doShift "3"]
+               , [ spotifySelector --> doShift "4"]]
 
 -- Layout
 
