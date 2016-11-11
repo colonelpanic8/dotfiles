ee10889607c032fa8262f931fa309c10fe35d1f2
Ivan Malison <IvanMalison@gmail.com>
2016-11-10 21:09:24 -0800

    [Shell] Add pavolume function

--- dotfiles/lib/shellenv/linux.sh
+++ dotfiles/lib/shellenv/linux.sh
@@ -1,1 +1,6 @@
 command_exists 'open' || command_exists 'xdg-open' && alias open='xdg-open'
+
+pavolume () {
+	pactl list sinks | grep '^[[:space:]]Volume:' | \
+    head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,'
+}
