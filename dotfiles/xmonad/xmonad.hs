90efe1bc83715e0a410d8e5bfd266bc68eeb83c2
Ivan Malison <IvanMalison@gmail.com>
2016-11-14 05:50:26 -0600

    [XMonad] Add accordion layout

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -25,6 +25,7 @@ import XMonad.Config ()
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.FadeInactive
+import XMonad.Layout.Accordion
 import XMonad.Layout.BoringWindows
 import XMonad.Layout.LayoutCombinators
 import XMonad.Layout.LayoutModifier
@@ -113,7 +114,7 @@ selectToggle = do
 
 -- TODO: Figure out how to disable focus follows mouse for magicFocus
 layouts = multiCol [1, 1] 2 0.01 (-0.5) ||| Full ||| Tall 1 (3/100) (1/2) |||
-          Tall 1 (3/100) (3/4)
+          Tall 1 (3/100) (3/4) ||| Accordion
 
 myLayoutHook = avoidStruts . minimize . boringAuto . mkToggle (MIRROR ?? EOT) .
                mkToggle (LIMIT ?? EOT) . mkToggle (GAPS ?? EOT) .
