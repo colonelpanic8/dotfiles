d411a2ce2da9ac6c4b80867554a6f0453bba4e48
Ivan Malison <IvanMalison@gmail.com>
2016-11-29 11:29:22 -0800

    Fix longlines in rofi_start_service.sh

--- dotfiles/lib/bin/rofi_restart_service.sh
+++ dotfiles/lib/bin/rofi_restart_service.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env zsh
 
 systemctl --user | tail -n +2 | head -n -6 |
-	egrep -v "sys-devices" | get_cols 1 | rofi -dmenu -i | xargs systemctl --user restart
+	egrep -v "sys-devices" | get_cols 1 | rofi -dmenu -i |
+	xargs systemctl --user restart
