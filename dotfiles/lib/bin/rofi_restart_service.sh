e7bd2b2bb0805c1717524486bed151e5215402ff
Ivan Malison <IvanMalison@gmail.com>
2016-12-05 15:44:37 -0800

    [Linux] Show enabled state of services

--- dotfiles/lib/bin/rofi_restart_service.sh
+++ dotfiles/lib/bin/rofi_restart_service.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
 systemctl --user list-unit-files | tail -n +2 | head -n -2 |
-	get_cols 1 | rofi -dmenu -i | xargs systemctl --user restart
+	rofi -dmenu -i | get_cols 1 | xargs systemctl --user restart
