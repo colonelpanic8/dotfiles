cd21eb25ddced6e0781e902fde47b4de49b2adad
Ivan Malison <IvanMalison@gmail.com>
2016-12-07 13:47:58 -0800

    [Linux] Fixup type_system_password

--- dotfiles/lib/bin/type_system_password.sh
+++ dotfiles/lib/bin/type_system_password.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
-PASSWORD="$(keepasshttp.py --get -u "http://system.com" | jq '.[].password' | unescape.py)"
-sleep .1s
-xdotool type "$PASSWORD"
+#!/usr/bin/env zsh
+
+xdotool type --clearmodifiers "$(keepass_system_password)"
