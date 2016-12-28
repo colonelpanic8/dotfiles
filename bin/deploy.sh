25733425efadc51e0cdb8f21e60ea1708a3172a6
Ivan Malison <IvanMalison@gmail.com>
2016-12-28 04:50:17 -0800

    Mehfdsa

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -39,16 +39,6 @@ rm -rf out/**/* || exit 0
 # Run our compile script
 doCompile
 
-# Now let's go have some fun with the cloned repo
-cd out
-git config user.name "$COMMIT_AUTHOR_NAME"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
-
-# Commit the "changes", i.e. the new version.
-# The delta will show diffs between new and old versions.
-git add --all .
-git commit -m "Deploy to GitHub Pages: ${SHA}"
-
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
@@ -58,5 +48,15 @@ openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out 
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
+
+# Now let's go have some fun with the cloned repo
+cd out
+git config user.name "$COMMIT_AUTHOR_NAME"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+# Commit the "changes", i.e. the new version.
+# The delta will show diffs between new and old versions.
+git add --all .
+git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 git push "$SSH_REPO" "$TARGET_BRANCH"
