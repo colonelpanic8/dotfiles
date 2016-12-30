fd57bc0a61c369a8ef0636d1e3618e2f0641b8f2
Ivan Malison <IvanMalison@gmail.com>
2016-12-29 20:44:32 -0800

    [Emacs] Always use selected sink

--- dotfiles/lib/bin/set_volume.sh
+++ dotfiles/lib/bin/set_volume.sh
@@ -2,4 +2,4 @@
 
 pulseaudio-ctl "$@"
 
-notify-send " " -i notification-audio-volume-high -h int:value:$(SINK=1 pavolume) -h string:synchronous:volume
+notify-send " " -i notification-audio-volume-high -h int:value:$(pavolume) -h string:synchronous:volume
--- dotfiles/lib/shellenv/linux.sh
+++ dotfiles/lib/shellenv/linux.sh
@@ -1,6 +1,10 @@
 command_exists 'open' || command_exists 'xdg-open' && alias open='xdg-open'
 
+pasink () {
+	pacmd list-sinks | grep '* index' | get_cols ' -1'
+}
+
 pavolume () {
 	pactl list sinks | grep '^[[:space:]]Volume:' | \
-    head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,'
+    head -n $(( $(pasink) + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,'
 }
