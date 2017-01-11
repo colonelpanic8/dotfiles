5692dd3f010837ce3acbdf5d2d1c462b25e42006
Ivan Malison <IvanMalison@gmail.com>
2017-01-11 12:50:27 -0800

    [Linux] Allow setting of profile for hangouts

--- dotfiles/lib/bin/start_hangouts.sh
+++ dotfiles/lib/bin/start_hangouts.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env sh
 
-google-chrome-stable --profile-directory=Default --app-id=knipolnnllmklapflnccelgolnpehhpl
+HANGOUTS_PROFILE="Default"
+
+[ -e $HOME/.hangouts_profile ] && HANGOUTS_PROFILE="$(cat $HOME/.hangouts_profile)"
+
+google-chrome-stable \
+	--profile-directory="$HANGOUTS_PROFILE" \
+	--app-id=knipolnnllmklapflnccelgolnpehhpl
