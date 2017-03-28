52394888cbde47cbfbddba1d8ed06aec11ac4532
Ivan Malison <IvanMalison@gmail.com>
2017-03-28 14:55:42 -0700

    [XMonad] Add a binding for skippy-xd

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -724,6 +724,7 @@ addKeys conf@XConfig { modMask = modm } =
     , ((hyper, xK_9), spawn "start_synergy.sh")
     , ((hyper, xK_8), spawn "rofi_paswitch.sh")
     , ((hyper, xK_slash), spawn "toggle_taffybar.sh")
+    , ((hyper, xK_space), spawn "skippy-xd")
 
     -- Media keys
 
