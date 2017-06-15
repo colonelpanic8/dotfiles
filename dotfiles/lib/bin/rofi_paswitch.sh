91e1f5a42b878411978303283038d5589b66ee98
Ivan Malison <IvanMalison@gmail.com>
2017-06-14 20:15:58 -0700

    [Linux] Handle two digit output sink ids

--- dotfiles/lib/bin/rofi_paswitch.sh
+++ dotfiles/lib/bin/rofi_paswitch.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 
-pahelper.sh | tr -d '\n' | tr '>' '|' | rofi -i -dmenu -sep '|' | grep -Eo ' [0-9]' | xargs pahelper.sh
+pahelper.sh | tr -d '\n' | tr '>' '|' | rofi -i -dmenu -sep '|' | grep -Eo ' [0-9]*' | xargs pahelper.sh
