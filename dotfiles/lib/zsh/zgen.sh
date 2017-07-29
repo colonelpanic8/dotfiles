12207b8d82730618ee00a82e246e94265b361cff
Ivan Malison <IvanMalison@gmail.com>
2017-07-29 00:39:04 -0700

    [zsh] Use pure prompt

--- dotfiles/lib/zsh/zgen.sh
+++ dotfiles/lib/zsh/zgen.sh
@@ -6,13 +6,15 @@ source "$ZGEN_PATH/zgen.zsh"
 
 # if the init scipt doesn't exist
 if ! zgen saved; then
-	zgen oh-my-zsh
+	zgen oh-my-zsh plugins/safe-paste
 	zgen oh-my-zsh plugins/git
 	zgen oh-my-zsh plugins/sudo
 	zgen oh-my-zsh plugins/command-not-found
 	zgen oh-my-zsh plugins/pip
 	zgen load zsh-users/zsh-syntax-highlighting
 	zgen load kennethreitz/autoenv
+	zgen load mafredri/zsh-async
+	zgen load sindresorhus/pure
 
 	# generate the init script from plugins above
 	zgen save
