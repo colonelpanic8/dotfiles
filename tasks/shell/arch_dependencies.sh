d4f6e57331c4d1a5bbb28f95fe10f0a18ff893b7
Ivan Malison <IvanMalison@gmail.com>
2017-08-10 12:10:59 -0400

    [Linux] Add dependencies for avahi/mdns

--- tasks/shell/arch_dependencies.sh
+++ tasks/shell/arch_dependencies.sh
@@ -8,11 +8,11 @@ indirect_expand() {
 ESSENTIAL=(
 	"lightdm-git" "zsh" "git" "emacs-git" "autoconf" "pkg-config" "pyenv"
 	"autoconf" "automake" "the_silver_searcher" "jdk8-openjdk" "openjdk8-doc"
-	"openjdk8-src" "ghc" "cabal-install" "sparkleshare" "rofi" "rxvt-unicode"
+	"openjdk8-src" "ghc" "cabal-install" "sparkleshare" "rofi" "termite"
 	"rofi-pass" "xorg-xrandr" "spotify" "htop" "rcm" "networkmanager-applet"
 	"pulseaudio-ctl" "pasystray" "xclip" "copyq" "notification-daemon" "pass"
 	"pavucontrol" "xsettingsd-git" "udisks" "udiskie" "python-pip"
-	"gnome-keyring" "ntp" "nss-mdns"
+	"gnome-keyring" "ntp" "nss-mdns" "avahi" "nss-mdns"
 )
 
 NEEDED=(
@@ -40,6 +40,11 @@ NVIDIA=(
 
 MACBOOK=(
 	"broadcom-wl-dkms" "pommed-light" "batterymon-clone"
+)
+
+SERVICES=(
+	"sshd.socket" "nvidia-persistenced.service" "NetworkManager.service"
+	"--user vncserver@:1" "autorandr.service" "avahi-daemon.service"
 )
 
 install_deps() {
