9e6a395aa9e37e81b21217a49a3257b8e3c94c1b
Ivan Malison <IvanMalison@gmail.com>
2017-08-23 16:04:38 -0700

    [Emacs] Switch from package.el to straight.el

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -1,11 +1,22 @@
 ;; -*- no-byte-compile: t -*-
-(require 'package)
-(package-initialize)
+(let ((bootstrap-file (concat user-emacs-directory "straight/bootstrap.el"))
+      (bootstrap-version 2))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-(setq load-prefer-newer t)
+(straight-use-package 'use-package)
+(require 'use-package)
+(setq use-package-enable-imenu-support t
+	  use-package-always-ensure t)
 
 (setq custom-file "~/.emacs.d/custom-before.el")
-(when (file-exists-p custom-file) (load custom-file))
+(setq load-prefer-newer t)
 
 ;; If this isn't here and there's a problem with init, graphical emacs
 ;; is super annoying.
@@ -13,11 +24,40 @@
   (setq mac-option-modifier 'meta)
   (setq mac-command-modifier 'super))
 
-(setq use-package-enable-imenu-support t)
+;; Without this, org can behave very strangely
+(use-package org
+  :init
+  ;; Taken from https://github.com/raxod502/radian/blob/master/radian-emacs/radian-org.el
+  (defun radian--org-git-version ()
+    "Return the abbreviated SHA for the Org Git repo."
+    (let ((default-directory (concat user-emacs-directory
+                                     "straight/repos/org/")))
+      (if (executable-find "git")
+          (with-temp-buffer
+            ;; Returns the shortest prefix of the SHA for HEAD that is
+            ;; unique, down to a minimum of 4 characters (see
+            ;; git-rev-parse(1)).
+            (call-process "git" nil '(t nil) nil
+                          "rev-parse" "--short" "HEAD")
+            (if (> (buffer-size) 0)
+                (string-trim (buffer-string))
+              ;; This shouldn't happen, unless somehow Org is not
+              ;; actually a Git repo.
+              "revision unknown"))
+        ;; This also shouldn't happen, because how would you have
+        ;; gotten Org in the first place, then? But the real world
+        ;; sucks and we have to account for stuff like this.
+        "git not available")))
+  (defalias #'org-git-version #'radian--org-git-version)
+  (defun org-release () "N/A")
+  (provide 'org-version)
+  (with-eval-after-load 'org
+    (defalias #'org-git-version #'radian--org-git-version)))
 
 (let ((debug-on-error t))
   (org-babel-load-file
-   (concat (file-name-directory load-file-name) "README.org")))
+   (straight-transaction
+     (concat (file-name-directory load-file-name) "README.org"))))
 
 ;; Local Variables:
 ;; flycheck-disabled-checkers: (emacs-lisp-checkdoc)
