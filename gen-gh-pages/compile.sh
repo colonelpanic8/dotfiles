aeac3e02689c29190ba9146dd895fda4d5179a82
Kat Huang <kkathuang@gmail.com>
2017-09-10 03:37:33 -0700

    [travis] Set emacs binary explicityly

--- gen-gh-pages/compile.sh
+++ gen-gh-pages/compile.sh
@@ -6,7 +6,9 @@ THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TARGET=$(readlink -f "$THIS_DIR/../dotfiles/emacs.d/README.org")
 
 git clone https://github.com/rejeep/evm.git "$HOME/.evm"
+evm config path /tmp
 evm install emacs-25.1-travis --use --skip
+export EMACS="$(evm bin)"
 
 curl -fsSkL https://raw.github.com/cask/cask/master/go | python
 
