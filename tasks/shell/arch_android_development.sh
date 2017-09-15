04ca1bfdba10986135e3f17840ffa0b4b6b187bd
Ivan Malison <IvanMalison@gmail.com>
2017-09-15 03:16:20 -0700

    [Linux] Fix android setup

--- tasks/shell/arch_android_development.sh
+++ tasks/shell/arch_android_development.sh
@@ -1,12 +1,13 @@
+#!/usr/bin/env zsh
+
 pacaur -S android-platform android-sdk android-sdk-platform-tools android-sdk-build-tools --noconfirm --noedit --needed
 
-groupadd sdkusers
-gpasswd -a $(whoami) asdkusers
-
-sudo -v
+sudo groupadd asdkusers
+sudo gpasswd -a "$(whoami)" asdkusers
 
 sudo chown -R :asdkusers /opt/android-sdk/
 sudo chmod -R g+w /opt/android-sdk/
-sudo newgrp sdkusers
+sudo newgrp asdkusers
 
-pacaur -S android-platform-25
+pacaur -S android-platform-25 --noconfirm --noedit --needed
+pacaur -S android-studio --noconfirm --noedit --needed
