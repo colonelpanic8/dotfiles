aa502f8d730c03841a289d009d7c51e48ebddbb8
Ivan Malison <IvanMalison@gmail.com>
2017-10-19 21:02:36 -0700

    Add macbook brightness

---
+++ dotfiles/lib/bin/brightness.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env zsh
+
+volnoti-show $(sudo macbook_brightness.py -c "$@" -p)
--- dotfiles/lib/bin/show_brightness.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-actual="$(cat /sys/class/backlight/intel_backlight/actual_brightness)"
-max="$(cat /sys/class/backlight/intel_backlight/max_brightness)"
-
-temp="$(( $actual * 100 ))"
-percentage="$(( $temp/$max ))"
-
-volnoti-show "$percentage"
