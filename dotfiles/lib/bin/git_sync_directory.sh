7371f0aeaae6af46d143a82bdcbf1f845d7a51a0
Ivan Malison <IvanMalison@gmail.com>
2018-06-14 16:00:30 -0700

    [Linux] Start using git-sync to auto-synchronize directories

--- dotfiles/lib/bin/git_sync_directory.sh
+++ dotfiles/lib/bin/git_sync_directory.sh
@@ -1,11 +1,22 @@
-#!/usr/bin/env bash
-
-cd "$1"
+#!/usr/bin/env zsh
 
 echo "Syncing $1"
-git-sync
-enable_git_sync.sh
+cd "$1"
 
-while changedFile=$(inotifywait ./ -r -e modify,move,create,delete --format "%w%f" --exclude '\.git' 2>/dev/null); do
-	git check-ignore "$changedFile" >> /dev/null || git-sync
+counter=0
+while true; do
+	changedFile=$(inotifywait ./ -r -e modify,move,create,delete --format "%w%f" --exclude '\.git' -t 20 2>/dev/null)
+	if [ -z "$changedFile" ]
+	then
+		counter=$((counter+1))
+		if [ $counter -gt 4 ]; then
+			git-sync
+			counter=0
+		else
+			git-sync > /dev/null
+		fi
+	else
+		echo "Syncing for: $changedFile"
+		{ git check-ignore "$changedFile" > /dev/null; } || git-sync
+	fi
 done
