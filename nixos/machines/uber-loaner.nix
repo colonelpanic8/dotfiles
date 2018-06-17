a748a6c85a700cf2aae0e37d59faf29f6dc79f40
Ivan Malison <IvanMalison@gmail.com>
2018-06-16 19:18:11 -0700

    [NixOS] Use grub for uber-loaner

--- nixos/machines/uber-loaner.nix
+++ nixos/machines/uber-loaner.nix
@@ -9,6 +9,14 @@
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "sd_mod" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.enable = true;
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/EFI";
+    fsType = "vfat";
+  };
 
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/2958f04b-8387-4a0c-abc1-f12036c53581";
@@ -20,8 +28,13 @@
       fsType = "tmpfs";
     };
 
+  environment.systemPackages = [
+    pkgs.linuxPackages.rtl8814au
+  ];
+
   swapDevices = [ ];
 
   nix.maxJobs = lib.mkDefault 4;
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  users.extraUsers.root.initialHashedPassword = "";
 }
