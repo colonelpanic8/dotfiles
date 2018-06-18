3b2eccfa64f21ba4cf345ce14caf19199b50be59
Ivan Malison <IvanMalison@gmail.com>
2018-06-18 14:33:55 -0700

    [Linux] Always look up ipv4 address with localip

--- dotfiles/lib/shellenv/functions.sh
+++ dotfiles/lib/shellenv/functions.sh
@@ -459,7 +459,10 @@ function localip {
             ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'
             ;;
         'Linux')
-            hostname --ip-address
+			ip -4 addr | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep -v 127.0.0.1 | head -n 1
+			# This was a nicer solution, but it returns ipv6 addresses on
+            # machines that have them:
+			# hostname --ip-address
             ;;
     esac
 }
