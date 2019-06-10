b611d8a1e2c6b4c0adf078b4c84ebe0fe10df93a
Ivan Malison <IvanMalison@gmail.com>
2019-06-10 14:53:41 -0700

    Add taffybar shell nix files

---
+++ dotfiles/config/taffybar/shell.nix
@@ -0,0 +1,7 @@
+let
+  pkgs = import <nixpkgs> { overlays = [
+    (import ./taffybar/overlay.nix) (import ../xmonad/overlay.nix)
+  ]; };
+  source = pkgs.lib.sourceByRegex ./. ["taffybar.hs" "imalison-taffybar.cabal"];
+  imalison-taffybar = pkgs.haskellPackages.callCabal2nix "imalison-taffybar" source { };
+in pkgs.haskellPackages.shellFor { packages = _: [imalison-taffybar pkgs.haskellPackages.taffybar]; }
