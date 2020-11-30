994a2a2e43308d41830a6ebe632c37a8896e009e
Ivan Malison <IvanMalison@gmail.com>
2020-11-29 17:36:27 -0800

    [Linux] XKB Map loading tweaks

--- dotfiles/lib/bin/load_xkb_map.sh
+++ dotfiles/lib/bin/load_xkb_map.sh
@@ -8,4 +8,13 @@ echo "loading keymap from $XKBMAPFILE"
 
 xkbcomp -I"$XKBDIR" "$XKBMAPFILE" "${DISPLAY%%.*}"
 
-unset XKBDIR XKBMAPFILE
+XKB_DEVICE_DIR="$XKBDIR/devices"
+
+for file in "$XKB_DEVICE_DIR"/*
+do
+	DEVICE_ID=$(xinput | grep "$(basename $file)" | head -n 1 | grep -Eo "id=[0-9]*" | grep -Eo "[0-9]*")
+	echo $file
+	echo device
+	echo "$DEVICE_ID"
+	# [ ! -z "$DEVICE_ID" ] && xkbcomp -I"$XKBDIR" -i "$DEVICE_ID" "$file" "${DISPLAY%%.*}"
+done
