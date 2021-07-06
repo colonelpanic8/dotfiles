75c2c4c467584db60c974214ff02c77e732b1061
Ivan Malison <IvanMalison@gmail.com>
2021-07-06 01:29:45 -0600

    [NixOS] Build quill from source

--- nixos/overlays.nix
+++ nixos/overlays.nix
@@ -30,18 +30,38 @@ in
     runVend = true;
   };
 
-  quill = self.stdenv.mkDerivation (rec {
-    name = "quill";
-    version = "0.2.0";
-    phases = [ "installPhase" ];
-    installPhase = ''
-      mkdir -p $out/bin
-      cp $src $out/bin/quill
-      chmod a+x $out/bin/quill
-    '';
-    src = self.fetchurl {
-      url = "https://github.com/dfinity/quill/releases/download/v${version}/quill-linux-x86_64";
-      sha256 = "1msgj7y8b0bh0sq14msa5jqwfn9yzwiin5l276qz0h0wjqmnndbs";
+  quill = with self; rustPlatform.buildRustPackage rec {
+    name = "quill-${version}";
+    version = "0baa53c175";
+
+    src = fetchFromGitHub {
+      owner = "dfinity";
+      repo = "quill";
+      rev = "6f3117c2b97195eb2b0059011063929553bc18ea";
+      sha256 = "1nr31jmhx1hnqba6n5dk8qal9r843lwss6ldcdyvwz4cmybndnna";
+      # date = 2021-06-18T08:16:07+02:00;
     };
-  });
+
+    ic = fetchFromGitHub {
+      owner = "dfinity";
+      repo = "ic";
+      rev = "779549eccfcf61ac702dfc2ee6d76ffdc2db1f7f";
+      sha256 = "1r31d5hab7k1n60a7y8fw79fjgfq04cgj9krwa6r9z4isi3919v6";
+    };
+
+    registry = "file://local-registry";
+
+    preBuild = ''
+      export REGISTRY_TRANSPORT_PROTO_INCLUDES=${ic}/rs/registry/transport/proto
+      export IC_BASE_TYPES_PROTO_INCLUDES=${ic}/rs/types/base_types/proto
+      export IC_PROTOBUF_PROTO_INCLUDES=${ic}/rs/protobuf/def
+      export IC_NNS_COMMON_PROTO_INCLUDES=${ic}/rs/nns/common/proto
+      export PROTOC=${protobuf}/bin/protoc
+    '';
+
+    cargoSha256 = "0h756lkvyqwsw3984dm0ys6qrdl22isg2zh2mmzqyw8220fgdzph";
+
+    nativeBuildInputs = [ pkg-config protobuf ];
+    buildInputs = [ openssl protobuf ];
+  };
 }
