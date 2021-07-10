42a568eba1780ed1732e478a803f4a070be622b9
Ivan Malison <IvanMalison@gmail.com>
2021-07-10 15:27:14 -0600

    [NixOS] Add users.nix

---
+++ nixos/users.nix
@@ -0,0 +1,39 @@
+{ pkgs }:
+{
+  security.sudo.wheelNeedsPassword = false;
+  users.extraUsers = let
+    extraGroups = [
+      "audio"
+      "adbusers"
+      "disk"
+      "docker"
+      "networkmanager"
+      "plugdev"
+      "systemd-journal"
+      "video"
+      "wheel"
+    ];
+    userDefaults = {
+      inherit extraGroups;
+      group = "users";
+      isNormalUser = true;
+      createHome = true;
+      shell = pkgs.zsh;
+    };
+  in {
+    imalison = userDefaults // {
+      name = "imalison";
+      uid = 1000;
+      home = "/home/imalison";
+      shell = pkgs.zsh;
+    };
+    kat = userDefaults // {
+      name = "kat";
+      uid = 1001;
+      home = "/home/kat";
+      shell = pkgs.zsh;
+    };
+  };
+
+  nix.trustedUsers = ["imalison" "kat"];
+}
