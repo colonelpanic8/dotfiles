f41dbb83588d3dd6723bb0d46b6ecbc73842316d
Ivan Malison <IvanMalison@gmail.com>
2021-07-10 15:30:43 -0600

    [NixOS] Remove users from base.nix

--- nixos/base.nix
+++ nixos/base.nix
@@ -171,7 +171,7 @@
   # TODO: Add a comment explaining what this does.
   services.gnome.at-spi2-core.enable = true;
 
-    services.xserver = {
+  services.xserver = {
     exportConfiguration = true;
     enable = true;
     layout = "us";
@@ -197,41 +197,5 @@
         systemctl --user import-environment GDK_PIXBUF_MODULE_FILE DBUS_SESSION_BUS_ADDRESS PATH
       '';
     };
-    };
-
-    users.extraUsers = let
-    extraGroups = [
-      "audio"
-      "adbusers"
-      "disk"
-      "docker"
-      "networkmanager"
-      "plugdev"
-      "systemd-journal"
-      "video"
-      "wheel"
-    ];
-    userDefaults = {
-      inherit extraGroups;
-      group = "users";
-      isNormalUser = true;
-      createHome = true;
-      shell = pkgs.zsh;
-    };
-  in {
-    imalison = userDefaults // {
-      name = "imalison";
-      uid = 1000;
-      home = "/home/imalison";
-      shell = pkgs.zsh;
-    };
-    kat = userDefaults // {
-      name = "kat";
-      uid = 1001;
-      home = "/home/kat";
-      shell = pkgs.zsh;
-    };
   };
-
-  nix.trustedUsers = ["imalison" "kat"];
 }
