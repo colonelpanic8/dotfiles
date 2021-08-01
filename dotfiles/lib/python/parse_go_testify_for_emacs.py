8a3457a28c55f28611b3c60db9f5470abe75c1d7
Ivan Malison <IvanMalison@gmail.com>
2021-08-01 01:04:45 -0600

    Remove random python scripts

--- dotfiles/lib/python/parse_go_testify_for_emacs.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import parse_go_testify_not_equal
-import sys
-import json
-
-if __name__ == '__main__':
-    actual, expected = parse_go_testify_not_equal.get_strings(sys.stdin.read())
-    print json.dumps({"actual": actual, "expected": expected})
