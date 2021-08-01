50d7801d67e2b1e363df2552f5825ed7cf8375ef
Ivan Malison <IvanMalison@gmail.com>
2021-08-01 01:38:59 -0600

    Delete more old setup files

--- dotfiles/lib/login.sh
+++
@@ -1,6 +0,0 @@
-source "$HOME/.lib/shellpath.sh"
-
-# XXX: This is duplicated from env with a different var to avoid issues with /etc/profile
-command_exists python && setup_unless_environment_variable_exists LOGIN_PATH_SETUP_DONE _setup_path
-
-source_directory_files "$HOME/.lib/login"
