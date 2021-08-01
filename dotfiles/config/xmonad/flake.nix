00d03e9774047188c0f232bf6854584fd94c64a4
Ivan Malison <IvanMalison@gmail.com>
2021-08-01 01:12:53 -0600

    [XMonad] Add flake for imalison-xmonad

---
+++ dotfiles/config/xmonad/flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    flake-utils.url = github:numtide/flake-utils;
+    xmonad-contrib = {
+      url = path:./xmonad-contrib;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    xmonad = {
+      url = path:./xmonad;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, flake-utils, nixpkgs, xmonad, xmonad-contrib }:
+  let
+    overlay = import ./overlay.nix;
+    overlays = [ overlay xmonad.overlay xmonad-contrib.overlay ];
+  in flake-utils.lib.eachDefaultSystem (system:
+  let pkgs = import nixpkgs { inherit system overlays; config.allowBroken = true; };
+  in
+  rec {
+    devShell = pkgs.haskellPackages.shellFor {
+      packages = p: [ p.imalison-xmonad ];
+    };
+    defaultPackage = pkgs.haskellPackages.imalison-xmonad;
+  }) // { inherit overlay overlays; } ;
+}
