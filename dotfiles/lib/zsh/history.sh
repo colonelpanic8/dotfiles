46d938a1710a0697183fe6b9a6ceddfb951eedf3
Ivan Malison <IvanMalison@gmail.com>
2021-08-02 00:31:18 -0600

    Remove useless stuff in history.sh

--- dotfiles/lib/zsh/history.sh
+++ dotfiles/lib/zsh/history.sh
@@ -3,8 +3,6 @@ export HISTFILE="$(readlink -f $HOME/.zsh_history)"
 HISTSIZE=10000000
 SAVEHIST=10000000
 
-shell_contains "$HISTFILE" SparkleShare && echo "Warning: sparkleshare in zsh history"
-
 setopt share_history # share command history data
 setopt extended_history
 setopt inc_append_history
@@ -12,12 +10,3 @@ setopt hist_expire_dups_first
 setopt hist_ignore_dups # ignore duplication command history list
 setopt hist_ignore_space
 setopt hist_verify
-
-# TODO: Explain what some of this does..
-
-bindkey -e
-bindkey '\ew' kill-region
-bindkey -s '\el' "ls\n"
-bindkey '^r' history-incremental-search-backward
-bindkey "^[[5~" up-line-or-history
-bindkey "^[[6~" down-line-or-history
