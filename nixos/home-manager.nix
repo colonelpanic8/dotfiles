8f9eeb392be1828f9f3b9fe5b78a4e6e6ef83a5d
Ivan Malison <IvanMalison@gmail.com>
2021-08-13 02:29:06 -0600

    [NixOS] Add service for notifications-tray-icon

--- nixos/home-manager.nix
+++ nixos/home-manager.nix
@@ -106,6 +106,21 @@
     };
   };
 
+  systemd.user.services.notifications-tray-icon = {
+    Unit = {
+      Description = "Notifications tray icon";
+      After = [ "graphical-session-pre.target" "tray.target" ];
+      PartOf = [ "graphical-session.target" ];
+    };
+
+    Install = { WantedBy = [ "graphical-session.target" ]; };
+
+    Service = {
+      ExecStart = "${pkgs.haskellPackages.notifications-tray-icon}/bin/notifications-tray-icon  --github-token-pass github-token";
+      Restart = "always";
+      RestartSec = 3;
+    };
+  };
 
   systemd.user.services.picom = {
     Unit = {
