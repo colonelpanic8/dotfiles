6b25eb24fb13fb540beaa367de971db8e12b6240
Ivan Malison <IvanMalison@gmail.com>
2021-08-20 19:28:51 -0600

    [NixOS] Remove forAll

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -32,49 +32,38 @@
     self, nix, nixpkgs, nixos-hardware, home-manager, taffybar, xmonad,
     xmonad-contrib, notifications-tray-icon
   }:
-  let forAll = ({ ... }: {
-    nix = {
-      extraOptions = ''
-        experimental-features = nix-command flakes ca-references
-      '';
-      registry.nixpkgs.flake = nixpkgs;
+  let
+    mkConfig =
+      args@
+      { system ? "x86_64-linux"
+      , baseModules ? []
+      , modules ? []
+      , specialArgs ? {}
+      , ...
+      }:
+    nixpkgs.lib.nixosSystem (args // {
+      inherit system;
+      modules = baseModules ++ modules;
+      specialArgs = { inherit inputs; } // specialArgs;
+    });
+    machineFilenames = builtins.attrNames (builtins.readDir ./machines);
+    machineNameFromFilename = filename: builtins.head (builtins.split "\\." filename);
+    mkConfigurationParams = filename: {
+      name = machineNameFromFilename filename;
+      value = {
+        modules = [ (./machines + ("/" + filename)) ];
+      };
     };
-    nixpkgs.overlays = [
-      nix.overlay xmonad.overlay xmonad-contrib.overlay
-      notifications-tray-icon.overlay (import ../dotfiles/config/taffybar/overlay.nix)
-      (import ./overlay.nix)
-    ] ++ taffybar.overlays;
-    imports = [
-      home-manager.nixosModule
-    ];
-    home-manager.useGlobalPkgs = true;
-    home-manager.useUserPackages = true;
-    home-manager.users.imalison = import ./home-manager.nix;
-  });
-  mkConfig = args@{ system ? "x86_64-linux", baseModules ? [ forAll ], modules ? [], specialArgs ? {}, ... }:
-  nixpkgs.lib.nixosSystem (args // {
-    inherit system;
-    modules = baseModules ++ modules;
-    specialArgs = { inherit inputs; } // specialArgs;
-  });
-  machineFilenames = builtins.attrNames (builtins.readDir ./machines);
-  machineNameFromFilename = filename: builtins.head (builtins.split "\\." filename);
-  mkConfigurationParams = filename: {
-    name = machineNameFromFilename filename;
-    value = {
-      modules = [ (./machines + ("/" + filename)) ];
-    };
-  };
-  defaultConfigurationParams =
-    builtins.listToAttrs (map mkConfigurationParams machineFilenames);
-  customParams = {
-    biskcomp = {
-      system = "aarch64-linux";
-    };
-    air-gapped-pi = {
-      system = "aarch64-linux";
-    };
-  };
+    defaultConfigurationParams =
+      builtins.listToAttrs (map mkConfigurationParams machineFilenames);
+      customParams = {
+        biskcomp = {
+          system = "aarch64-linux";
+        };
+        air-gapped-pi = {
+          system = "aarch64-linux";
+        };
+      };
   in
   {
     nixosConfigurations = builtins.mapAttrs (machineName: params:
