a746a2957586b5ba87b04b27ddce01e6e3171cae
Ivan Malison <IvanMalison@gmail.com>
2021-09-12 23:13:12 -0600

    [rofi] Fix colorful theme

--- dotfiles/config/rofi/colorful/launcher.sh
+++ dotfiles/config/rofi/colorful/launcher.sh
@@ -12,7 +12,7 @@
 # style_7     style_8     style_9     style_10    style_11    style_12
 
 theme="style_1"
-dir="$HOME/.config/rofi/colorful"
+dir="$HOME/.config/rofi/launchers/colorful"
 
 # dark
 ALPHA="#00000000"
