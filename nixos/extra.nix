2efcc180399b57bf9be8b1f7547326b2c70d9c4e
Ivan Malison <IvanMalison@gmail.com>
2022-04-19 00:54:56 -0600

    [NixOS] Add some extra stuff

--- nixos/extra.nix
+++ nixos/extra.nix
@@ -3,8 +3,11 @@
   programs.sway.enable = true;
   services.xserver.windowManager.i3.enable = true;
   environment.systemPackages = with pkgs; [
+    rust-analyzer
     asciidoctor
     vscode
+    roomeqwizard
+    razergenie
     signal-desktop
     gource
     gimp
