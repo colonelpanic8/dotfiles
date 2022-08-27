dc8015977748fb84303b68d0217b6bac81d14303
Ivan Malison <IvanMalison@gmail.com>
2022-08-27 09:05:42 -0600

    Use dccsillag's fork with animation support

--- nixos/overlay.nix
+++ nixos/overlay.nix
@@ -2,9 +2,9 @@ final: prev: {
   picom = prev.picom.overrideAttrs(_: {
     src = prev.fetchFromGitHub {
       repo = "picom";
-      owner = "jonaburg";
-      rev = "a8445684fe18946604848efb73ace9457b29bf80";
-      sha256 = "sha256-R+YUGBrLst6CpUgG9VCwaZ+LiBSDWTp0TLt1Ou4xmpQ=";
+      owner = "dccsillag";
+      rev = "51b21355696add83f39ccdb8dd82ff5009ba0ae5";
+      sha256 = "sha256-crCwRJd859DCIC0pEerpDqdX2j8ZrNAzVaSSB3mTPN8==";
     };
   });
 }
