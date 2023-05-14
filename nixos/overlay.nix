eede6c0c7ff28c4c6921788275ad1ca49f0dbf6b
Ivan Malison <IvanMalison@gmail.com>
2023-05-14 15:14:19 -0600

    [NixOS] Add broken expressvpn overlay

--- nixos/overlay.nix
+++ nixos/overlay.nix
@@ -7,4 +7,11 @@ final: prev: {
       sha256 = "sha256-crCwRJd859DCIC0pEerpDqdX2j8ZrNAzVaSSB3mTPN8==";
     };
   });
+
+  expressvpn = prev.expressvpn.overrideAttrs(_: {
+    src = prev.fetchurl {
+      url = "https://www.expressvpn.works/clients/linux/expressvpn_3.46.0.7-1_amd64.deb";
+      hash = "sha256-v0rr1s32jj79A8IGfJgSGJVlz7rSnZYn4ealOpqee0w=";
+    };
+  });
 }
