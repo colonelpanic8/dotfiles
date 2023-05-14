a08ebf4ccc073913ffacd9a2199f33c30e4f1f0f
Ivan Malison <IvanMalison@gmail.com>
2023-05-14 15:16:25 -0600

    [NixOS] Add ffmpeg as extra package

--- nixos/extra.nix
+++ nixos/extra.nix
@@ -2,12 +2,15 @@
 {
   programs.sway.enable = true;
   services.xserver.windowManager.i3.enable = true;
+  services.expressvpn.enable = true;
   environment.systemPackages = with pkgs; [
+    ffmpeg
     rust-analyzer
     asciidoctor
     vscode
     roomeqwizard
     razergenie
+    expressvpn
     signal-desktop
     gource
     gimp
