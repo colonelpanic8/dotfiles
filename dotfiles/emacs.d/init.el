bde118bcce86f384dcba72e0a533bcfd14426550
Ivan Malison <IvanMalison@gmail.com>
2023-05-25 02:25:36 -0600

    Tweaks

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -53,35 +53,7 @@
 
 ;; Without this, org can behave very strangely
 (use-package org
-  :defer t
-  :init
-  (progn
-    ;; Taken from https://github.com/raxod502/radian/blob/master/radian-emacs/radian-org.el
-    (defun radian--org-git-version ()
-      "Return the abbreviated SHA for the Org Git repo."
-      (let ((default-directory (concat user-emacs-directory
-                                       "straight/repos/org/")))
-        (if (executable-find "git")
-            (with-temp-buffer
-              ;; Returns the shortest prefix of the SHA for HEAD that is
-              ;; unique, down to a minimum of 4 characters (see
-              ;; git-rev-parse(1)).
-              (call-process "git" nil '(t nil) nil
-                            "rev-parse" "--short" "HEAD")
-              (if (> (buffer-size) 0)
-                  (string-trim (buffer-string))
-                ;; This shouldn't happen, unless somehow Org is not
-                ;; actually a Git repo.
-                "revision unknown"))
-          ;; This also shouldn't happen, because how would you have
-          ;; gotten Org in the first place, then? But the real world
-          ;; sucks and we have to account for stuff like this.
-          "git not available")))
-    (defalias #'org-git-version #'radian--org-git-version)
-    (defun org-release () "N/A")
-    (provide 'org-version)
-    (with-eval-after-load 'org
-      (defalias #'org-git-version #'radian--org-git-version))))
+  :defer t)
 
 (let ((debug-on-error t))
   (org-babel-load-file
