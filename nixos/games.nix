9808c678414ea66e6f5fd28bd140d1e345d2bc25
Ivan Malison <IvanMalison@gmail.com>
2023-06-13 22:19:25 -0600

    [NixOS] Setup for rocket league in linux

--- nixos/games.nix
+++ nixos/games.nix
@@ -1,7 +1,12 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    # steam
+    steam
+    heroic
   ];
-  boot.extraModulePackages = with pkgs; [ xboxdrv ];
+  boot.extraModulePackages = with pkgs; [
+    xboxdrv
+  ];
+  # hardware.xpadneo.enable = true;
+  hardware.xone.enable = true;
 }
