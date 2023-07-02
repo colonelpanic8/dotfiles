333fc3fe409ea2c8635cdd299877a3f3e642f2ab
Kat Huang <kkathuang@gmail.com>
2023-07-02 13:51:38 -0600

    [NixOS] Add home-manager and obsidian syncing for kat

--- nixos/kat.nix
+++ nixos/kat.nix
@@ -4,4 +4,31 @@
     bitwarden
     obsidian
   ];
+
+  home-manager.users.kat = { pkgs, config, ... }: {
+    services.gpg-agent = {
+      enable = true;
+      defaultCacheTtl = 8 * 60 * 60;
+      maxCacheTtl = 8 * 60 * 60;
+      enableSshSupport = true;
+      pinentryFlavor = "qt";
+    };
+
+    services.kdeconnect = {
+      enable = true;
+      indicator = true;
+    };
+
+    services.git-sync = {
+      enable = true;
+      repositories = {
+        obsidian = {
+          path = config.home.homeDirectory + "/obsidian";
+          uri = "git@github.com:katandtonic/obsidian.git";
+        };
+      };
+    };
+
+    home.stateVersion = "23.11";
+  };
 }
