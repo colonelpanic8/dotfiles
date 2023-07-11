26c3fb51132e2665300581998876a85c9bac8b04
Kat Huang <kkathuang@gmail.com>
2023-07-11 12:06:52 -0600

    [NixOS] Use AMD gpu drivers on jay-lenovo

--- nixos/machines/jay-lenovo.nix
+++ nixos/machines/jay-lenovo.nix
@@ -12,24 +12,28 @@
   hardware.enableRedistributableFirmware = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.kernelModules = [ "amdgpu" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" "amdgpu" ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-  services.xserver.enable = true;
-  services.xserver.libinput.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  networking.networkmanager.enable = true;
+
+  services.xserver = {
+      enable = true;
+      libinput.enable = true;
+      displayManager.sddm.enable = true;
+      desktopManager.plasma5.enable = true;
+      videoDrivers = [ "amdgpu" ];
+  };
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/cb96b029-df61-45d3-905b-a9435bf446df";
     fsType = "ext4";
   };
-
 
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/1C5A-4FBB";
