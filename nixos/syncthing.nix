37a415952d2cf308b215fb5a3308486032afccba
Ivan Malison <IvanMalison@gmail.com>
2023-08-03 21:50:55 -0600

    [NixOS] Update nixquick address

--- nixos/syncthing.nix
+++ nixos/syncthing.nix
@@ -5,13 +5,13 @@
     settings = {
       folders = {
         sync = {
-          path = "~/sync/";
+          path = "~/";
         };
       };
       devices = {
         ryzen-shine = { id = "IYCR3JH-BDZ4N3F-ZBUETYR-4N2UILK-FAEJJ5B-YUYMZQ3-YN63E5T-I2EHCAK"; };
         biskcomp = { id = "63SZFEC-AVA7MAL-MRJCZXN-3J4MKY6-52CK37Q-EQDN27P-7LOTYFX-UTL7YA3"; };
-        nixquick = { id = "LSRG4WP-GHW7UV6-IUL5PQB-NWDWVIZ-R7PBGEH-RCHLFRR-7JBPQU2-WFEGPQP"; };
+        nixquick = { id = "6OAGJ3J-3P3R33R-ICHXDWX-IDDCUU7-ESN3Y65-2OGZMWL-R647V7N-4TA6IQM"; };
       };
     };
   };
