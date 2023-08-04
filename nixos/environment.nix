84cf1566312154d7da33ac1f5173399812316330
Ivan Malison <IvanMalison@gmail.com>
2023-08-04 00:42:19 -0600

    [NixOS] [starship] Move starship config to environment.nix

--- nixos/environment.nix
+++ nixos/environment.nix
@@ -31,6 +31,13 @@ with lib;
 
     programs.starship = {
       enable = true;
+      settings = '''
+        add_newline = false
+
+        [character]
+        success_symbol = "[➜](bold green)"
+        error_symbol = "[➜](bold red)"
+      '''
     };
 
     environment = {
