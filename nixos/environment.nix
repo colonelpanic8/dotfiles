625c9aaaff191fa87820bf23fa7140cd5b06ce86
Ivan Malison <IvanMalison@gmail.com>
2023-08-04 00:49:34 -0600

    [NixOS] Try again...

--- nixos/environment.nix
+++ nixos/environment.nix
@@ -31,13 +31,13 @@ with lib;
 
     programs.starship = {
       enable = true;
-      settings = """
-        add_newline = false
-
-        [character]
-        success_symbol = \"[➜](bold green)\"
-        error_symbol = \"[➜](bold red)\"
-      """;
+      settings = {
+        add_newline = false;
+        character = {
+          success_symbol = "[➜](bold green)";
+          error_symbol = "[➜](bold red)";
+        };
+      };
     };
 
     environment = {
