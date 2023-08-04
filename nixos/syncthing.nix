fbfdc597b158661323a6c560049448562b356ecd
Ivan Malison <IvanMalison@gmail.com>
2023-08-03 21:44:13 -0600

    [NixOS] Try to get syncthing working

--- nixos/syncthing.nix
+++ nixos/syncthing.nix
@@ -2,9 +2,14 @@
 {
   services.syncthing = {
     enable = true;
+    folders = {
+      sync = {
+        path = "/home/syncthing/sync/";
+      };
+    };
     settings = {
       devices = {
-        ryzen-shine = { id = "63NX4LF-DHG7DLP-P6BHDKL-QL7TFXE-3DEGJ6G-TUBNXYS-UMI35KF-BV67WQP"; };
+        ryzen-shine = { id = "IYCR3JH-BDZ4N3F-ZBUETYR-4N2UILK-FAEJJ5B-YUYMZQ3-YN63E5T-I2EHCAK"; };
         biskcomp = { id = "63SZFEC-AVA7MAL-MRJCZXN-3J4MKY6-52CK37Q-EQDN27P-7LOTYFX-UTL7YA3"; };
         nixquick = { id = "LSRG4WP-GHW7UV6-IUL5PQB-NWDWVIZ-R7PBGEH-RCHLFRR-7JBPQU2-WFEGPQP"; };
       };
