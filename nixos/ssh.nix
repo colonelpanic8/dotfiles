67d72d17903d3206af0037f91b9fe9a11a087bba
Ivan Malison <IvanMalison@gmail.com>
2023-08-21 17:25:41 -0600

    [NixOS] Add known hosts for github and gitlab

--- nixos/ssh.nix
+++ nixos/ssh.nix
@@ -22,4 +22,17 @@
       PasswordAuthentication = false;
     };
   };
+
+  programs.ssh = {
+    knownHosts = {
+      github = {
+        hostNames = ["github.com"];
+        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+      };
+      gitlab = {
+        hostNames = ["gitlab.com"];
+        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
+      };
+    };
+  };
 }
