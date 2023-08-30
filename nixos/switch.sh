87ea0bd124b2506d9d0ec2724d1a81e79a6defd6
Ivan Malison <IvanMalison@gmail.com>
2023-08-30 14:17:48 -0600

    [NixOS] Add fix update inputs issue

--- nixos/switch.sh
+++ nixos/switch.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env sh
-nix flake lock --update-input taffybar
-nix flake lock --update-input imalison-taffybar
-nix flake lock --update-input xmonad
-nix flake lock --update-input xmonad-contrib
 sudo nixos-rebuild switch --flake '.#' --impure "$@"
