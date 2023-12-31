a75824ee2a5d09e579d0792d103ac4fe5601e189
Ivan Malison <IvanMalison@gmail.com>
2023-12-30 22:42:43 -0700

    [NixOS] Programatically get the list of users

--- nixos/expire-home-manager-generations.sh
+++ nixos/expire-home-manager-generations.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-users=("root" "imalison" "kat" "dean" "alex" "will" "mike")
-
-for user in "${users[@]}"; do
-    sudo su - $user -c 'home-manager expire-generations -15days'
+for user in $(awk -F':' '{ if ($3 >= 1000 && $7 !~ /nologin|false|sync|shutdown|halt/) print $1 }' /etc/passwd); do
+    echo $user
+    sudo su - $user -c 'home-manager expire-generations -1days'
 done
