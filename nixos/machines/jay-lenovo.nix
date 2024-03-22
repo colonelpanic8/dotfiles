49d98cbca1c786a0318f1b70d11311bb8b83c74b
Ivan Malison <IvanMalison@gmail.com>
2024-03-21 18:11:06 -0600

    [NixOS] Fix for jay-lenovo

--- nixos/machines/jay-lenovo.nix
+++ nixos/machines/jay-lenovo.nix
@@ -33,8 +33,6 @@
   services.xserver = {
       enable = true;
       libinput.enable = true;
-      displayManager.sddm.enable = true;
-      desktopManager.plasma5.enable = true;
       videoDrivers = [ "amdgpu" ];
   };
 
