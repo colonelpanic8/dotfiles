a8a66916f41d56d75ea035d928c769a4bd1603f0
Ben Corner <benjamin.j.corner@gmail.com>
2024-05-28 22:17:40 -0500

    Add shellAliases, set backupFileExtension in ben.nix.

--- nixos/ben.nix
+++ nixos/ben.nix
@@ -1,12 +1,20 @@
 { pkgs, inputs, config, makeEnable, ... }:
 makeEnable config "modules.ben" true {
   home-manager.users.ben = {
+    home-manager = {
+      backupFileExtension = "backup";
+    };
+
     programs.zsh = {
       enable = true;
       shellAliases = {
-        ll = "ls -alF";
-        la = "ls -A";
         l = "ls -CF";
+        la = "ls -A";
+        ll = "ls -lh";
+        lla = "ls -alh";
+        ls = "ls --color=auto";
+        gts = "git status";
+        gtl = "git log";
       };
     };
   };
