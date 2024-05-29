9d900057f6f4dca981001546f5194e1916f977cc
Ben Corner <benjamin.j.corner@gmail.com>
2024-05-29 01:03:49 -0500

    Working ben.nix file.
    Probably unrelated, had to remove ~/.zshrc and ~/.zprofile in order for
    nixos-rebuild switch to work.

--- nixos/ben.nix
+++ nixos/ben.nix
@@ -1,7 +1,7 @@
 { pkgs, inputs, config, makeEnable, ... }:
 makeEnable config "modules.ben" true {
+  home-manager.backupFileExtension = "backup"; # Add this line
   home-manager.users.ben = {
-    home-manager.backupFileExtension = "backup"; # Add this line
     programs.zsh = {
       enable = true;
       shellAliases = {
