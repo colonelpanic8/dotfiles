26fb168ec52cd03ec02ef66e4db9f055f67ea2c0
Ivan Malison <IvanMalison@gmail.com>
2024-07-02 17:18:03 -0600

    [NixOS] Set backup extension for home-manager

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -18,6 +18,7 @@
     };
     home-manager.useGlobalPkgs = true;
     home-manager.useUserPackages = true;
+    home-manager.backupFileExtension = "backup";
 
     nix = rec {
       extraOptions = ''
