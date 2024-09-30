1003c33dee73bcbe92571445206c8fc9fd0e22b0
Ivan Malison <IvanMalison@gmail.com>
2024-09-30 15:19:29 -0600

    [NixOS] Use myModules.nvidia in jimi-hendnix

--- nixos/machines/jimi-hendnix.nix
+++ nixos/machines/jimi-hendnix.nix
@@ -19,9 +19,7 @@
 
   hardware.enableRedistributableFirmware = true;
 
-  # install nvidia drivers in addition to intel one
-  hardware.opengl.extraPackages = [ pkgs.linuxPackages.nvidia_x11.out ];
-  hardware.opengl.extraPackages32 = [ pkgs.linuxPackages.nvidia_x11.lib32 ];
+  myModules.nvidia.enable = true;
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
   boot.kernelModules = [ "kvm-intel" ];
