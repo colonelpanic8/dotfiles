4cb9c006d755f7f0966dc2d53cd92eae47f2bafb
Your Name <you@example.com>
2024-10-01 00:38:01 +0000

    [NixOS] railbird-sf tweaks

--- nixos/machines/railbird-sf.nix
+++ nixos/machines/railbird-sf.nix
@@ -12,57 +12,34 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
   boot.loader.systemd-boot.enable = true;
-
-  hardware.opengl.extraPackages = [ pkgs.linuxPackages.nvidia_x11.out ];
-  hardware.opengl.extraPackages32 = [ pkgs.linuxPackages.nvidia_x11.lib32 ];
-  services.xserver = {
-    videoDrivers = [ "nvidia" ];
-  };
+  modules.postgres.enable = true;
 
   # Enable OpenGL
   hardware.opengl = {
     enable = true;
-    driSupport = true;
     driSupport32Bit = true;
   };
 
   hardware.nvidia = {
-
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
     powerManagement.enable = false;
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
     powerManagement.finegrained = false;
 
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
-
     # Enable the Nvidia settings menu,
 	  # accessible via `nvidia-settings`.
     nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   features.full.enable = false;
   myModules.base.enable = true;
   myModules.desktop.enable = true;
   myModules.xmonad.enable = false;
-  myModules.gnome.enable = true;
   myModules.code.enable = true;
   myModules.syncthing.enable = true;
   myModules.fonts.enable = true;
-  myModules.plasma.enable = false;
+  myModules.plasma.enable = true;
+  myModules.nvidia.enable = true;
   myModules.gitea-runner.enable = true;
 
   fileSystems."/" =
@@ -87,7 +64,7 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  
+
   home-manager.users = forEachUser {
      home.stateVersion = "23.11";
   };
