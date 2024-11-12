be7448b710c69a422102984fcbc654fbd27332c0
Ivan Malison <IvanMalison@gmail.com>
2024-11-11 18:39:32 -0700

    [NixOS] Use open nvidia drivers

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -6,9 +6,8 @@ makeEnable config "myModules.nvidia" false {
   ];
   hardware.nvidia-container-toolkit = {
     enable = true;
-    mount-nvidia-executables = true;
   };
-  hardware.nvidia.open = false;
+  hardware.nvidia.open = true;
   hardware.graphics.extraPackages = [ pkgs.linuxPackages.nvidia_x11.out ];
   hardware.graphics.extraPackages32 = [ pkgs.linuxPackages.nvidia_x11.lib32 ];
   hardware.graphics.enable32Bit = true;
