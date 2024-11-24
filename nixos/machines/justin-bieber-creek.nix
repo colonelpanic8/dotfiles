f972642cfa1d642d96a109231d82ab57a5b59c2b
Ivan Malison <IvanMalison@gmail.com>
2024-11-24 09:10:44 +0000

    [NixOS] Add justin-bieber-creek

---
+++ nixos/machines/justin-bieber-creek.nix
@@ -0,0 +1,42 @@
+{ config, lib, pkgs, forEachUser, ... }:
+
+{
+  imports = [
+    ../configuration.nix
+  ];
+
+  myModules.fonts.enable = true;
+  myModules.base.enable = true;
+  myModules.desktop.enable = true;
+  
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+  boot.loader.systemd-boot.enable = true;
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/453d28a1-26f2-4b25-ac72-c6d301fd0bb8";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/184E-E5E8";
+      fsType = "vfat";
+    };
+
+  swapDevices = [ ];
+
+  networking.hostName = "justin-bieber-creek";
+
+  networking.useDHCP = lib.mkDefault true;
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  system.stateVersion = "23.05";
+
+  home-manager.users = forEachUser {
+    home.stateVersion = "23.05";
+  };
+}
