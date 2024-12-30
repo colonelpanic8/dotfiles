7d76728651ec8bd044ca28655e6dcc6757b5571c
Ivan Malison <IvanMalison@gmail.com>
2024-12-30 00:56:02 -0700

    Revert "[NixOS] Use plugins path"
    This reverts commit 957b94e1cc38da4430d38cea997ac6244a1bfaa4.

--- nixos/k3s.nix
+++ nixos/k3s.nix
@@ -54,7 +54,7 @@ in {
         plugins."io.containerd.grpc.v1.cri" = {
           enable_cdi = true;
           cdi_spec_dirs = [ "/var/run/cdi" ];
-          cni.bin_dir = "${plugins-path}/bin";
+          cni.bin_dir = "/opt/cni/bin";
         };
       };
     };
