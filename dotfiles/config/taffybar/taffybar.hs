2a303c445c91196677d17b48a621f78bc4b6f58d
Ivan Malison <IvanMalison@gmail.com>
2025-01-15 00:20:10 -0700

    [taffybar] Change adele -> adele in taffybar

--- dotfiles/config/taffybar/taffybar.hs
+++ dotfiles/config/taffybar/taffybar.hs
@@ -218,7 +218,7 @@ main = do
           [ ( "uber-loaner"
             , baseConfig { endWidgets = laptopEndWidgets }
             )
-          , ( "adele"
+          , ( "adell"
             , baseConfig { endWidgets = laptopEndWidgets }
             )
           , ( "stevie-nixos"
