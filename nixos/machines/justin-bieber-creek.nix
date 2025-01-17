4f573be120fe895fe0601daaec618614b336fa82
Ivan Malison <IvanMalison@gmail.com>
2025-01-17 01:59:55 -0700

    [NixOS] More home-assistant fix

--- nixos/machines/justin-bieber-creek.nix
+++ nixos/machines/justin-bieber-creek.nix
@@ -16,12 +16,12 @@
 
   services.home-assistant = {
     enable = true;
+    extraComponents = [
+      "esphome"
+      "met"
+      "radio_browser"
+    ];
     config = {
-      extraComponents = [
-        "esphome"
-        "met"
-        "radio_browser"
-      ];
       config = {
         default_config = {};
         met = {};
