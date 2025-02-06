94e3c08f8818d91b140c528961fda75110ab82dd
Ivan Malison <IvanMalison@gmail.com>
2025-02-06 13:00:37 -0700

    [NixOS] Reindent strixi-minaj

--- nixos/machines/strixi-minaj.nix
+++ nixos/machines/strixi-minaj.nix
@@ -1,3 +1,4 @@
+
 { config, lib, pkgs, inputs, forEachUser, ... }:
 
 {
@@ -42,12 +43,6 @@
   virtualisation.libvirtd.enable = true;
   virtualisation.spiceUSBRedirection.enable = true;
 
-  virtualisation.virtualbox.host = {
-    enable = true;
-    enableKvm = true;
-    addNetworkInterface = false;
-  };
-
   services.xserver.dpi = 96;
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.initrd.availableKernelModules = [ "vmd" "xhci_pci" "thunderbolt" "nvme" "usbhid" "usb_storage" "sd_mod" ];
@@ -60,28 +55,28 @@
 
   services.asusd.enable = true;
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/fc06a54c-cc45-423a-914b-8dfcb5939106";
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/fc06a54c-cc45-423a-914b-8dfcb5939106";
     fsType = "ext4";
-    };
+  };
 
-    fileSystems."/boot" =
-      { device = "/dev/disk/by-uuid/B28A-829A";
-      fsType = "vfat";
-      };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/B28A-829A";
+    fsType = "vfat";
+  };
 
-      swapDevices =
-        [ { device = "/dev/disk/by-uuid/27f277a0-b552-43a0-904d-625e48922bb9"; }
-        ];
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/27f277a0-b552-43a0-904d-625e48922bb9"; }
+  ];
 
-        networking.hostName = "strixi-minaj";
+  networking.hostName = "strixi-minaj";
 
-        powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
-        hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-        home-manager.users = forEachUser {
-          home.stateVersion = "23.05";
-        };
+  home-manager.users = forEachUser {
+    home.stateVersion = "23.05";
+  };
 
-        system.stateVersion = "23.05";
+  system.stateVersion = "23.05";
 }
