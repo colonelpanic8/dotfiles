7c25d8d5780807cae6a6db2010011de40cc80295
Ivan Malison <IvanMalison@gmail.com>
2025-08-14 02:35:13 -0600

    [NixOS] Bump rumno patch hash

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -159,7 +159,7 @@
           # Rumno service PR
           (bootstrapPkgs.fetchpatch {
             url = "https://github.com/NixOS/nixpkgs/pull/433540.patch";
-            hash = "sha256-6JP2XzqQniPT2PR3+s+IaGfw1/ft91Dikx5PC9pUGuk=";
+            hash = "sha256-zXtiUBYtGdhhkEhJlnmQ4Qk2CEUiSWWQ/2UipwmL7jM=";
           })
         ];
       };
