18168dbc591c1bc246c463ed343afe9abb143721
Ivan Malison <IvanMalison@gmail.com>
2026-02-12 19:14:19 -0800

    dotfiles: add rofi agentic skill launcher binding

--- dotfiles/config/xmonad/xmonad.hs
+++ dotfiles/config/xmonad/xmonad.hs
@@ -1100,6 +1100,7 @@ addKeys conf@XConfig { modMask = modm } =
     , ((hyper, xK_space), spawn "skippy-xd")
     , ((hyper, xK_i), spawn "rofi_select_input.hs")
     , ((hyper, xK_o), spawn "rofi_paswitch")
+    , ((hyper, xK_y), spawn "rofi_agentic_skill")
     , ((modm, xK_e), spawn "emacsclient --eval '(emacs-everywhere)'")
 
     -- Media keys
