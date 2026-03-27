46e36d62b02c945ff6462d1d3ddd46e20d34699d
Ivan Malison <IvanMalison@gmail.com>
2026-03-26 21:27:20 -0700

    fix(nixos): patch quill ledger.did during build

--- nixos/internet-computer.nix
+++ nixos/internet-computer.nix
@@ -1,8 +1,28 @@
 { pkgs, ... }:
+let
+  quillIcSrc = pkgs.fetchFromGitHub {
+    owner = "dfinity";
+    repo = "ic";
+    rev = "2f9ae6bf5eafed03599fd29475100aca9f78ae81";
+    hash = "sha256-QWJFsWZ9miWN4ql4xFXMQM1Y71nzgGCL57yAa0j7ch4=";
+  };
+
+  quillFixed = pkgs.quill.overrideAttrs (old: {
+    preBuild = (old.preBuild or "") + ''
+      # The vendored git dependency resolves ../ledger.did from source-git-*,
+      # not from the vendor root where nixpkgs currently copies it.
+      for dir in /build/quill-*-vendor/source-git-*; do
+        if [ -d "$dir" ]; then
+          cp ${quillIcSrc}/rs/rosetta-api/icp_ledger/ledger.did "$dir/ledger.did"
+        fi
+      done
+    '';
+  });
+in
 {
   environment.systemPackages = with pkgs; [
     ic-keysmith
-    quill
+    quillFixed
     quill-qr
   ];
 }
