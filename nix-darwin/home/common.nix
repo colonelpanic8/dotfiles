14082c3dfe95a848b858991d8af0486e359b0ce8
Kat Huang <kkathuang@gmail.com>
2026-04-25 16:15:08 -0700

    codex: externalize generated system skills

--- nix-darwin/home/common.nix
+++ nix-darwin/home/common.nix
@@ -109,6 +109,10 @@ in {
     fi
   '';
 
+  home.activation.linkCodexSkills = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    ${pkgs.bash}/bin/bash "${libDir}/bin/setup_codex_skills" "${dotfilesDir}/agents/skills"
+  '';
+
   home.sessionPath = [
     "$HOME/.cargo/bin"
     "${libDir}/bin"
