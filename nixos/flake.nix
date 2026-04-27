c8941870e22b97ec17feb8276abf4571ace69f7d
Ivan Malison <IvanMalison@gmail.com>
2026-04-26 18:51:15 -0700

    nix: add shared binary caches

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -420,14 +420,32 @@
       ];
       extra-substituters = [
         "http://192.168.1.26:5050"
+        "https://ai.cachix.org"
+        "https://cache.nixos-cuda.org"
+        "https://nix-community.cachix.org"
+        "https://cuda-maintainers.cachix.org"
+        "https://numtide.cachix.org"
         "https://cache.flox.dev"
         "https://org-agenda-api.cachix.org"
         "https://colonelpanic8-dotfiles.cachix.org"
         "https://codex-cli.cachix.org"
         "https://claude-code.cachix.org"
       ];
+      extra-trusted-substituters = [
+        "https://ai.cachix.org"
+        "https://cache.nixos.org/"
+        "https://nix-community.cachix.org"
+        "https://cache.nixos-cuda.org"
+        "https://cuda-maintainers.cachix.org"
+        "https://numtide.cachix.org"
+      ];
       extra-trusted-public-keys = [
         "1896Folsom.duckdns.org:U2FTjvP95qwAJo0oGpvmUChJCgi5zQoG1YisoI08Qoo="
+        "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
+        "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
         "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
         "org-agenda-api.cachix.org-1:liKFemKkOLV/rJt2txDNcpDjRsqLuBneBjkSw/UVXKA="
         "colonelpanic8-dotfiles.cachix.org-1:O6GF3nptpeMFapX29okzO92eSWXR36zqW6ZF2C8P0eQ="
