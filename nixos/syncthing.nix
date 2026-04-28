3e05939ce3e827b52629b828b9d30e1dce7e6710
Ivan Malison <IvanMalison@gmail.com>
2026-04-28 15:48:35 -0700

    Add random Hyprland screensaver rotation

--- nixos/syncthing.nix
+++ nixos/syncthing.nix
@@ -2,47 +2,49 @@
   makeEnable,
   config,
   ...
-}: let
+}:
+let
   shared = import ../nix-shared/syncthing.nix;
   inherit (shared) devices allDevices;
 in
-  makeEnable config "myModules.syncthing" true {
-    system.activationScripts.syncthingPermissions = {
-      text = ''
-        chown -R syncthing:syncthing /var/lib/syncthing
-        chmod -R 2770 /var/lib/syncthing
-        mkdir -p /var/lib/syncthing/sync
-        mkdir -p /var/lib/syncthing/railbird
-      '';
+makeEnable config "myModules.syncthing" true {
+  system.activationScripts.syncthingPermissions = {
+    text = ''
+      mkdir -p /var/lib/syncthing/sync
+      mkdir -p /var/lib/syncthing/sync/Screensaver/use
+      mkdir -p /var/lib/syncthing/railbird
+      chown -R syncthing:syncthing /var/lib/syncthing
+      chmod -R 2770 /var/lib/syncthing
+    '';
+  };
+  systemd.services.syncthing = {
+    serviceConfig = {
+      AmbientCapabilities = "CAP_CHOWN";
+      CapabilityBoundingSet = "CAP_CHOWN";
     };
-    systemd.services.syncthing = {
-      serviceConfig = {
-        AmbientCapabilities = "CAP_CHOWN";
-        CapabilityBoundingSet = "CAP_CHOWN";
-      };
-    };
-    services.syncthing = {
-      enable = true;
-      settings = {
-        inherit devices;
-        folders = {
-          sync = {
-            path = "~/sync";
-            devices = allDevices;
-            ignorePerms = true;
-            copyOwnershipFromParent = true;
-          };
-          railbird = {
-            path = "~/railbird";
-            devices = allDevices;
-            ignorePerms = true;
-            copyOwnershipFromParent = true;
-          };
+  };
+  services.syncthing = {
+    enable = true;
+    settings = {
+      inherit devices;
+      folders = {
+        sync = {
+          path = "~/sync";
+          devices = allDevices;
+          ignorePerms = true;
+          copyOwnershipFromParent = true;
         };
-        options = {
-          relaysEnabled = true;
-          localAnnounceEnabled = true;
+        railbird = {
+          path = "~/railbird";
+          devices = allDevices;
+          ignorePerms = true;
+          copyOwnershipFromParent = true;
         };
       };
+      options = {
+        relaysEnabled = true;
+        localAnnounceEnabled = true;
+      };
     };
-  }
+  };
+}
