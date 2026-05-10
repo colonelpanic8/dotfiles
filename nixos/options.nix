2a036581c7293081843c1a6beef8a8d4de8e703c
Ivan Malison <IvanMalison@gmail.com>
2026-05-10 02:37:58 -0700

    Use host identity colors for tmux status

--- nixos/options.nix
+++ nixos/options.nix
@@ -42,9 +42,9 @@ in {
     '';
 
     environment.etc."tmux-host-style.conf".text = ''
-      set -g status-style "fg=#ffffff,bg=blue"
-      set -g status-left-style "fg=#ffffff,bg=blue"
-      set -g status-right-style "fg=#ffffff,bg=blue"
+      set -g status-style "fg=${hostIdentity.tmux.foreground},bg=${hostIdentity.tmux.background}"
+      set -g status-left-style "fg=${hostIdentity.tmux.foreground},bg=${hostIdentity.tmux.background}"
+      set -g status-right-style "fg=${hostIdentity.tmux.foreground},bg=${hostIdentity.tmux.background}"
     '';
   };
 }
