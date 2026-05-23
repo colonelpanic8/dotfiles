dde547f69489bc5cab836696de6728704e9fe377
Ivan Malison <IvanMalison@gmail.com>
2026-05-22 20:01:32 -0700

    Use programs.ssh.settings for Home Manager

--- nixos/home-manager.nix
+++ nixos/home-manager.nix
@@ -304,18 +304,18 @@ in {
     extraConfig = ''
       Include /home/imalison/config/dotfiles/ssh/config
     '';
-    matchBlocks = {
+    settings = {
       "*" = {
-        forwardAgent = true;
-        addKeysToAgent = "no";
-        compression = false;
-        serverAliveInterval = 0;
-        serverAliveCountMax = 3;
-        hashKnownHosts = false;
-        userKnownHostsFile = "~/.ssh/known_hosts";
-        controlMaster = "no";
-        controlPath = "~/.ssh/master-%r@%n:%p";
-        controlPersist = "no";
+        ForwardAgent = true;
+        AddKeysToAgent = "no";
+        Compression = false;
+        ServerAliveInterval = 0;
+        ServerAliveCountMax = 3;
+        HashKnownHosts = false;
+        UserKnownHostsFile = "~/.ssh/known_hosts";
+        ControlMaster = "no";
+        ControlPath = "~/.ssh/master-%r@%n:%p";
+        ControlPersist = "no";
       };
     };
   };
