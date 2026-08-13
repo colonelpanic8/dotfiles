afd47ab9f89d07f53b591ce7dcf58833a2cce1ba
Ivan Malison <IvanMalison@gmail.com>
2026-08-13 10:48:44 -0700

    Pin polished T3 Code managed fleet assembly

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -361,7 +361,7 @@
     # Pin by REV, never by branch: generated assembly branches move on rebuild.
     # Each rebuild also pushes a dated tag, so older revs stay fetchable.
     t3code-integration = {
-      url = "github:colonelpanic8/t3code/59dba05b29334e85a2a7134c3081d34b19e4d4ed";
+      url = "github:colonelpanic8/t3code/5a4927f359faa415c011a1971d5f7772cbec62f8";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-utils.follows = "flake-utils";
