661458c829197132cc66f7749539269d493cff16
Ivan Malison <IvanMalison@gmail.com>
2026-09-03 10:59:21 -0700

    emacs: silence Emacs 31 lexical-binding warnings

--- dotfiles/emacs.d/early-init.el
+++ dotfiles/emacs.d/early-init.el
@@ -1,3 +1,8 @@
 ;; -*- lexical-binding: t; -*-
 
+;; Emacs 31 reports missing lexical-binding cookies in third-party packages
+;; that are managed outside this checkout.  Project-owned files carry cookies.
+(defvar warning-suppress-log-types nil)
+(add-to-list 'warning-suppress-log-types '(files missing-lexbind-cookie))
+
 (setq package-enable-at-startup nil)
