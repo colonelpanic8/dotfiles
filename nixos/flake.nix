95a7ec7d9915e2ec09fc369f7f1b8e5457b9e94b
Ivan Malison <IvanMalison@gmail.com>
2026-09-07 15:15:41 -0700

    nixos: update flake inputs

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -360,7 +360,7 @@
     # Pin by REV, never by branch: generated assembly branches move on rebuild.
     # Each rebuild also pushes a dated tag, so older revs stay fetchable.
     t3code-integration = {
-      url = "github:colonelpanic8/t3code/78faaf345f2136f373f0b2e2a65c928a0318b6b6";
+      url = "github:colonelpanic8/t3code/e215320c19238dfeb8ad20b596f112b737e97bba";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-utils.follows = "flake-utils";
